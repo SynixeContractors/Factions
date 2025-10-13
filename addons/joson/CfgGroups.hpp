23a960b2d0e4bae12b69cfef2b0fc6ee7ba4e3b8
Patryk Kaniewski <p7tryk@gmail.com>
2025-10-13 22:01:52 +0200

    joson: remove sniper team

--- addons/joson/CfgGroups.hpp
+++ addons/joson/CfgGroups.hpp
@@ -3,18 +3,6 @@ class CfgGroups {
         class CLASS(t2_opfor) {
             name = "[T2] Joson Socialist Republic";
             @Groups(T2,OPFOR,Army_Combat);
-            class CLASS(T2_Army_Special) {
-                name = "Army (Custom)";
-                class CLASS(FireteamSniper) {
-                    name = "Sniper Team";
-                    side = 0;
-                    faction = "t2_opfor";
-                    icon = "\a3\ui_f\data\map\markers\nato\n_inf.paa";
-                    __COUNTER_RESET__
-                    GROUP_UNIT(CLASS(army_combat_Sniper),0);
-                    GROUP_UNIT(CLASS(army_combat_Marksman),0);
-                };
-            };
         };
     };
 };
