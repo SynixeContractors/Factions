b243760d6c429409630934cda7088bb40324d241
LtHave <141883390+LtHave@users.noreply.github.com>
2025-10-13 22:22:38 +0200

    Update addons/bdf/CfgGroups.hpp
    Co-authored-by: BrettMayson <brett@mayson.io>

--- addons/bdf/CfgGroups.hpp
+++ addons/bdf/CfgGroups.hpp
@@ -2,8 +2,8 @@ class CfgGroups {
     class EAST {
         class CLASS(t2_indep) {
             name = "[T2] Breznian Defense Force";
-            @Groups(T2,OPFOR,Army_Combat);
-            @Groups(T2,OPFOR,Army_Garrison);
+            @Groups(T2,INDEP,Army_Combat);
+            @Groups(T2,INDEP,Army_Garrison);
         };
     };
 };
