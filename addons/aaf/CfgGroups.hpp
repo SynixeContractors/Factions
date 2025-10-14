055673838d5d39487df0fb238b3e6228036f90e7
LtHave <141883390+LtHave@users.noreply.github.com>
2025-10-14 16:10:31 +0200

    aaf v0.1
    - adds AAF army and army garrison

---
+++ addons/aaf/CfgGroups.hpp
@@ -0,0 +1,9 @@
+class CfgGroups {
+    class EAST {
+        class CLASS(t2_opfor) {
+            name = "[T2] Altian Armed Force";
+            @Groups(T2,OPFOR,Army_Combat);
+            @Groups(T2,OPFOR,Army_Garrison);
+        };
+    };
+};
---
+++ addons/aaf/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT aaf
+#define COMPONENT_BEAUTIFIED Altian Armed Forces
+
+#include "..\main\script_mod.hpp"
+#include "..\main\script_macros.hpp"
