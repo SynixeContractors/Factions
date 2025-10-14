fe741afee677534fc976a114c356153fbfcb1939
LtHave <141883390+LtHave@users.noreply.github.com>
2025-10-14 16:30:29 +0200

    aaf v0.11
    - added National Reserves (Natres)

--- addons/aaf/CfgGroups.hpp
+++ addons/aaf/CfgGroups.hpp
@@ -4,6 +4,7 @@ class CfgGroups {
             name = "[T2] Altian Armed Force";
             @Groups(T2,OPFOR,Army_Combat);
             @Groups(T2,OPFOR,Army_Garrison);
+            @Groups(T2,OPFOR,natres)
         };
     };
 };
