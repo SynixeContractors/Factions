d8b8171aecd071fcaf00a54cd36b6540bc81c3fb
TWatson1162 <mildlyperturbedgamer25@gmail.com>
2025-10-19 03:15:23 +0900

    Karskoyan Armed Forces
    [DO NOT MERGE]
    
    No working groups (KAF is BluFor, Compositions are located in OpFor, everything else works fine, but do not merge until support for BluFor groups is added

---
+++ addons/kaf/CfgGroups.hpp
@@ -0,0 +1,9 @@
+class CfgGroups {
+    class EAST {
+        class CLASS(t3_blufor) {
+            name = "[T3] Karskoyan Armed Forces";
+            @Groups(T2,BLUFOR,Army_Combat);
+            @Groups(T2,BLUFOR,Army_Garrison);
+        };
+    };
+};
