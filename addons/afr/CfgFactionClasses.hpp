049f7a86884084672b9ae91330fdafbaafb06aaa
TWatson1162 <mildlyperturbedgamer25@gmail.com>
2025-10-20 15:40:48 +0900

    Armed Forces of Rajarta
    1st Push

---
+++ addons/afr/CfgFactionClasses.hpp
@@ -0,0 +1,8 @@
+class CfgFactionClasses {
+    class CLASS(t3_indep) {
+        SCOPE_DLC;
+        displayName = "[T3] Armed Forces of Rajarta";
+        priority = 1;
+        side = 2; // INDEP
+    };
+};
---
+++ addons/afr/CfgGroups.hpp
@@ -0,0 +1,9 @@
+class CfgGroups {
+    class EAST {
+        class CLASS(t3_opfor) {
+            name = "[T3] Armed Forces of Rajarta";
+            @Groups(T3,INDEP,Army_Combat);
+            @Groups(T3,INDEP,Army_Garrison);
+        };
+    };
+};
