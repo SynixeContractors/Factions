b26db49563000f9629ea2ade9a181b8b8ab42818
Patryk Kaniewski <p7tryk@gmail.com>
2025-10-26 12:38:46 +0100

    joson: t100

--- addons/joson/army_combat/CfgVehicles_T100.hpp
+++ addons/joson/army_combat/CfgVehicles_T100.hpp
@@ -3,9 +3,8 @@ class CLASS(T100): GCLASS(T100) {
   SCOPE_DLC;
   faction = QPCLASS(t3_opfor);
   side = 0;
-  //FIXME: make green
-  textureList[] = {"Hex",1};
-  hiddenSelectionstextures[] = {"\A3_Aegis\Armor_F_Aegis\MBT_02\Data\MBT_02_body_RUkhk_CO.paa","\A3_Aegis\Armor_F_Aegis\MBT_02\Data\MBT_02_turret_RUkhk_CO.paa","\A3_Aegis\Armor_F_Aegis\MBT_02\Data\MBT_02_RUkhk_CO.paa","\A3_Aegis\Armor_F_Aegis\Data\camonet_RUS_green_CO.paa"};
+  textureList[] = {"Green",1};
+  hiddenSelectionsTextures[] = {"\A3_Aegis\Armor_F_Aegis\MBT_02\Data\MBT_02_body_RUkhk_CO.paa","\A3_Aegis\Armor_F_Aegis\MBT_02\Data\MBT_02_turret_RUkhk_CO.paa","\A3_Aegis\Armor_F_Aegis\MBT_02\Data\MBT_02_RUkhk_CO.paa","\A3_Aegis\Armor_F_Aegis\Data\camonet_RUS_green_CO.paa"};
   crew = QCLASS(Crewman);
   typicalCargo[] = {QCLASS(Crewman), QCLASS(Crewman), QCLASS(Crewman)};
 };
