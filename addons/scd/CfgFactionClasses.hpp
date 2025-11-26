92f36802cd06fe6332ac024c047b36dca329d95b
LtHave <141883390+LtHave@users.noreply.github.com>
2025-11-26 20:04:30 +0100

    SCD v0.1
    Sons of the Coming Dawn
    T4 Militia faction

---
+++ addons/scd/CfgFactionClasses.hpp
@@ -0,0 +1,8 @@
+class CfgFactionClasses {
+    class CLASS(t3_opfor) {
+        SCOPE_DLC;
+        displayName = "[T4] Sons of the Coming Dawn";
+        priority = 1;
+        side = 0; // OPFOR
+    };
+};
---
+++ addons/scd/CfgGroups.hpp
@@ -0,0 +1,9 @@
+class CfgGroups {
+    class EAST {
+        class CLASS(t3_opfor) {
+            name = "[T4] Sons of the Coming Dawn";
+            @Groups(T3,OPFOR,Army_Combat);
+            @Groups(T3,OPFOR,Army_Garrison);
+        };
+    };
+};
