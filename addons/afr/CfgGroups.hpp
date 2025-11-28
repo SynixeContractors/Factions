85bb1bb906821908770f7c67223b57e00c28fe6b
TWatson1162 <mildlyperturbedgamer25@gmail.com>
2025-11-29 08:58:23 +0900

    group fix
    fixd problem, AFR now shows groups in Indep and not OpFor

--- addons/afr/CfgGroups.hpp
+++ addons/afr/CfgGroups.hpp
@@ -1,6 +1,6 @@
 class CfgGroups {
-    class EAST {
-        class CLASS(t3_opfor) {
+    class INDEP {
+        class CLASS(t3_indep) {
             name = "[T3] Armed Forces of Rajarta";
             @Groups(T3,INDEP,Army_Combat);
             @Groups(T3,INDEP,Army_Garrison);
