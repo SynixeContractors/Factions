5f4744fc3185bb2283c9988e6360d2bf1b24aa9d
LtHave <141883390+LtHave@users.noreply.github.com>
2025-11-28 21:56:05 +0100

    BMS v0.1
    - Adds Blackmire Solutions Army
    - Adds BMS Garrison
    - Adds BMS Recon

---
+++ addons/bms/CfgGroups.hpp
@@ -0,0 +1,9 @@
+class CfgGroups {
+    class EAST {
+        class CLASS(t3_opfor) {
+            name = "[T3] Blackmire Solutions";
+            @Groups(T3,OPFOR,Army_Combat);
+            @Groups(T3,OPFOR,Army_Garrison);
+        };
+    };
+};
