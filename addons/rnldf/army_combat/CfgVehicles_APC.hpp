a192220a478125d675ad75b2108bf832cebfd122
LtHave <141883390+LtHave@users.noreply.github.com>
2025-11-30 20:32:01 +0100

    RNLDF Vics
    - Adds vehicles to RNLDF

--- addons/rnldf/army_combat/CfgVehicles_APC.hpp
+++ addons/rnldf/army_combat/CfgVehicles_APC.hpp
@@ -4,8 +4,7 @@ class CLASS(OtokarUnarmed): GCLASS(OtokarUnarmed) {
     faction = QPCLASS(t3_opfor);
     side = 0;
     textureList[] = {"Green",1};
-    hiddenSelectionsTextures[] = {"A3\Armor_F_Enoch\apc_tracked_03\data\apc_tracked_03_ext_eaf_co.paa","A3\Armor_F_Enoch\apc_tracked_03\data\apc_tracked_03_ext2_eaf_co.paa","A3\Armor_F_Enoch\apc_tracked_03\Data\camonet_EAF_green_CO.paa","A3\Armor_F_Enoch\apc_tracked_03\data\cage_EAF_CO.paa"};
-    editorPreview = "\A3_Aegis\EditorPreviews_F_Aegis\Data\CfgVehicles\I_E_APC_tracked_03_cannon_v2_F.jpg";
+    editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\O_T_APC_Wheeled_02_unarmed_lxWS.jpg";
     crew = QCLASS(Crewman);
     typicalCargo[] = {QCLASS(Crewman), QCLASS(Crewman), QCLASS(Crewman)};
 };
@@ -16,8 +15,7 @@ class CLASS(OtokarHMG): GCLASS(OtokarHMG) {
     faction = QPCLASS(t3_opfor);
     side = 0;
     textureList[] = {"Green",1};
-    hiddenSelectionsTextures[] = {"\A3_Aegis\Armor_f_Aegis\APC_Wheeled_01\Data\APC_Wheeled_01_base_EAF_CO.paa","\A3_Aegis\Armor_f_Aegis\APC_Wheeled_01\Data\APC_Wheeled_01_adds_EAF_CO.paa","\A3_Aegis\Armor_f_Aegis\APC_Wheeled_01\Data\APC_Wheeled_01_tows_EAF_CO.paa","a3\Armor_F\Data\camonet_NATO_Green_CO.paa","a3\Armor_F\Data\cage_olive_CO.paa"};
-    editorPreview = "\A3_Aegis\EditorPreviews_F_Aegis\Data\CfgVehicles\Aegis_I_E_APC_Wheeled_01_cannon_v2_F.jpg";
+    editorPreview = "\lxws\editorpreviews_f_lxws\Data\CfgVehicles\O_T_APC_Wheeled_02_hmg_lxWS.jpg";
     crew = QCLASS(Crewman);
     typicalCargo[] = {QCLASS(Crewman), QCLASS(Crewman), QCLASS(Crewman)};
 };
@@ -28,8 +26,7 @@ class CLASS(OtokarRCWS): GCLASS(OtokarRCWS) {
     faction = QPCLASS(t3_opfor);
     side = 0;
     textureList[] = {"Green",1};
-    hiddenSelectionsTextures[] = {"\A3_Aegis\Armor_f_Aegis\APC_Wheeled_01\Data\APC_Wheeled_01_base_EAF_CO.paa","\A3_Aegis\Armor_f_Aegis\APC_Wheeled_01\Data\APC_Wheeled_01_adds_EAF_CO.paa","\A3_Aegis\Armor_f_Aegis\APC_Wheeled_01\Data\APC_Wheeled_01_tows_EAF_CO.paa","A3\Armor_F_Enoch\apc_tracked_03\Data\camonet_EAF_green_CO.paa","A3\Armor_F_Enoch\apc_tracked_03\data\cage_EAF_CO.paa","\A3_Aegis\Armor_f_Aegis\APC_Wheeled_01\Data\APC_Wheeled_01_lxws_EAF_CO.paa"};
-    editorPreview = "\A3_Aegis\EditorPreviews_F_Aegis\Data\CfgVehicles\Aegis_I_E_APC_Wheeled_01_cannon_v2_F.jpg";
+    editorPreview = "\A3\EditorPreviews_F_Exp\Data\CfgVehicles\O_T_APC_Wheeled_02_rcws_v2_ghex_F.jpg";
     crew = QCLASS(Crewman);
     typicalCargo[] = {QCLASS(Crewman), QCLASS(Crewman), QCLASS(Crewman)};
 };
