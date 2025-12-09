9aa9d3bdf7f14c8df38f50c8a3c244f1f5f90acf
TWatson1162 <mildlyperturbedgamer25@gmail.com>
2025-12-09 10:39:46 +0900

    Pls fix conflict????
    updated files from main

---
+++ addons/draf/CfgFactionClasses.hpp
@@ -0,0 +1,8 @@
+class CfgFactionClasses {
+    class CLASS(t2_opfor) {
+        SCOPE_DLC;
+        displayName = "[T2] Dal Riada Armed Forces";
+        priority = 1;
+        side = 0; // OPFOR
+    };
+};
