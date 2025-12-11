b364dfc1b6ee554e852b493a90ddeb9627e3514c
LtHave <141883390+LtHave@users.noreply.github.com>
2025-12-11 13:24:32 +0100

    BMS SOF (#44)
    * BDF SOF
    
    - Adds BDF SOF
    - Adds Gen4 NVG's to T2_standard pack
    - Adds frags to T2, T3 and T4 standard
    
    * Fix
    
    Fixed pack error
    
    * another quick fix
    
    fixxx
    
    * BMS SOF
    
    - Adds BMS SOF

--- addons/bms/CfgGroups.hpp
+++ addons/bms/CfgGroups.hpp
@@ -5,6 +5,7 @@ class CfgGroups {
             @Groups(T3,OPFOR,Army_Combat);
             @Groups(T3,OPFOR,Army_Garrison);
             @Groups(T3,OPFOR,army_recon);
+            @Groups(T3,OPFOR,special_forces);
         };
     };
 };
