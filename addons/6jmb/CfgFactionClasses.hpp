9281907de07c89975fbb6cf954e575afd489bdbb
LtHave <141883390+LtHave@users.noreply.github.com>
2025-12-11 13:27:45 +0100

    6jmb v0.1 (#46)
    Adds infantry for 6jmb

---
+++ addons/6jmb/CfgFactionClasses.hpp
@@ -0,0 +1,8 @@
+class CfgFactionClasses {
+    class CLASS(t4_indep) {
+        SCOPE_DLC;
+        displayName = "[T4] 6th June Martyr Brigade";
+        priority = 1;
+        side = 2; // INDEP
+    };
+};
---
+++ addons/6jmb/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT 6jmb
+#define COMPONENT_BEAUTIFIED 6th June Martyr Brigade
+
+#include "..\main\script_mod.hpp"
+#include "..\main\script_macros.hpp"
