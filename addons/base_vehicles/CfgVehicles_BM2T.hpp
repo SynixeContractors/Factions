c930f4bafd856281515e352221cf771d4e9d5780
Patryk Kaniewski <p7tryk@gmail.com>
2025-12-15 17:17:05 +0100

    base_vehicles: ZSU-35 stub

--- addons/base_vehicles/CfgVehicles_BM2T.hpp
+++ addons/base_vehicles/CfgVehicles_BM2T.hpp
@@ -421,17 +421,43 @@ class GCLASS(BM2_AA): O_APC_Tracked_02_AA_F {
     crew = "Civilian";
     typicalCargo[] = {"Soldier"};
     ace_refuel_fuelCapacity = 1200;
-    class TextureSources {
-        class Green {
-            displayName = "Green";
-            textures[] = {
-              "\A3_Aegis\Armor_F_Aegis\APC_Tracked_02\Data\APC_Tracked_02_ext_01_AA_RUkhk_CO.paa",
-              "\A3_Aegis\Armor_F_Aegis\APC_Tracked_02\Data\APC_Tracked_02_ext_02_RUkhk_CO.paa",
-              "\A3_Aegis\Armor_F_Aegis\APC_Tracked_02\Data\APC_Tracked_01_AA_Tower_RUkhk_CO.paa",
-              "\A3_Aegis\Armor_F_Aegis\Data\camonet_RUS_green_CO.paa",
-              "\A3_Aegis\Armor_F_Aegis\Data\cage_RUkhk_CO.paa"
-            };
-            factions[] = {};
-        };
+    class TextureSources
+    {
+      class Hex
+      {
+        displayName = "Hex Ochre";
+        textures[] = {"A3\Armor_F_Beta\APC_Tracked_02\Data\apc_tracked_02_ext_01_aa_hexarid_co.paa","A3\Armor_F_Beta\APC_Tracked_02\Data\apc_tracked_02_ext_02_hexarid_co.paa","A3\Armor_F_Beta\APC_Tracked_01\Data\apc_tracked_01_aa_tower_opfor_co.paa","A3\Armor_F\Data\camonet_CSAT_HEX_Desert_CO.paa","A3\armor_f\data\cage_csat_co.paa"};
+        factions[] = {};
+      };
+      class GreenHex
+      {
+        displayName = "Hex Green";
+        textures[] = {"A3\Armor_F_Exp\APC_Tracked_02\Data\APC_Tracked_02_ext_01_AA_ghex_CO.paa","A3\Armor_F_Exp\APC_Tracked_02\Data\APC_Tracked_02_ext_02_ghex_CO.paa","A3\Armor_F_Exp\APC_Tracked_02\Data\APC_Tracked_01_AA_Tower_ghex_CO.paa","A3\Armor_F\Data\camonet_CSAT_HEX_Green_CO.paa","A3\armor_f\data\cage_csat_green_CO.paa"};
+        factions[] = {};
+      };
+      class Green
+      {
+        displayName = "Green";
+        textures[] = {"\A3_Aegis\Armor_F_Aegis\APC_Tracked_02\Data\APC_Tracked_02_ext_01_AA_RUkhk_CO.paa","\A3_Aegis\Armor_F_Aegis\APC_Tracked_02\Data\APC_Tracked_02_ext_02_RUkhk_CO.paa","\A3_Aegis\Armor_F_Aegis\APC_Tracked_02\Data\APC_Tracked_01_AA_Tower_RUkhk_CO.paa","\A3_Aegis\Armor_F_Aegis\Data\camonet_RUS_green_CO.paa","\A3_Aegis\Armor_F_Aegis\Data\cage_RUkhk_CO.paa"};
+        factions[] = {};
+      };
+      class SFIA
+      {
+        displayName = "Mudwalker";
+        textures[] = {"lxws\vehicles_f_lxws\data\APC_Tracked_02\kamysh_aa_africa1_co.paa","lxws\vehicles_f_lxws\data\APC_Tracked_02\kamysh_africa2_co.paa","lxws\vehicles_f_lxws\data\APC_Tracked_02\kamysh_aa_africa3_co.paa","A3\Armor_F\Data\camonet_CSAT_HEX_Desert_CO.paa","A3\armor_f\data\cage_csat_co.paa"};
+        factions[] = {};
+      };
+      class Sand
+      {
+        displayName = "Sand";
+        textures[] = {"lxws\vehicles_f_lxws\data\APC_Tracked_02\kamysh_aa_sand1_co.paa","lxws\vehicles_f_lxws\data\APC_Tracked_02\kamysh_sand2_co.paa","lxws\vehicles_f_lxws\data\APC_Tracked_02\kamysh_aa_sand3.paa","A3\Armor_F\Data\camonet_CSAT_HEX_Desert_CO.paa","A3\armor_f\data\cage_csat_co.paa"};
+        factions[] = {};
+      };
+      class WoodlandHex
+      {
+        displayName = "Hex Woodland";
+        textures[] = {"\A3_Atlas\Armor_F_Atlas\APC_Tracked_02\Data\APC_Tracked_02_ext_01_AA_whex_CO.paa","\A3_Atlas\Armor_F_Atlas\APC_Tracked_02\Data\APC_Tracked_02_ext_02_whex_CO.paa","\A3_Atlas\Armor_F_Atlas\APC_Tracked_02\Data\APC_Tracked_02_AA_Tower_whex_CO.paa","\A3\Armor_F\Data\camonet_CSAT_Hex_Green_CO.paa","\A3_Aegis\Armor_F_Aegis\Data\cage_RUkhk_CO.paa"};
+        factions[] = {};
+      };
     };
-};
+  };
