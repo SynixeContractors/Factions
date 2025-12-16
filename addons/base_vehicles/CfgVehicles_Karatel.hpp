d5d698bbc06202b2e21b3b382db94e9c33a0ad4f
Patryk Kaniewski <p7tryk@gmail.com>
2025-12-16 18:49:03 +0100

    base_vehicles: add karatel

---
+++ addons/base_vehicles/CfgVehicles_Karatel.hpp
@@ -0,0 +1,35 @@
+class O_MRAP_02_F;
+class GCLASS(Karatel) : O_MRAP_02_F {
+    displayName = "Karatel";
+    faction = QGCLASS(base_vehicles);
+    SCOPE_DLC_VEHICLE;
+    EMPTY_INVENTORY;
+    crew = "Civilian";
+    typicalCargo[] = {"Soldier"};
+    ace_refuel_fuelCapacity = 190;
+    #include "textures/Karatel.hpp"
+};
+
+class O_MRAP_02_hmg_F;
+class GCLASS(Karatel_HMG) : O_MRAP_02_hmg_F {
+    displayName = "Karatel (HMG)";
+    faction = QGCLASS(base_vehicles);
+    SCOPE_DLC_VEHICLE;
+    EMPTY_INVENTORY;
+    crew = "Civilian";
+    typicalCargo[] = {"Soldier"};
+    ace_refuel_fuelCapacity = 190;
+    #include "textures/Karatel.hpp"
+};
+
+class O_MRAP_02_gmg_F;
+class GCLASS(Karatel_GMG) : O_MRAP_02_gmg_F {
+    displayName = "Karatel (GMG)";
+    faction = QGCLASS(base_vehicles);
+    SCOPE_DLC_VEHICLE;
+    EMPTY_INVENTORY;
+    crew = "Civilian";
+    typicalCargo[] = {"Soldier"};
+    ace_refuel_fuelCapacity = 190;
+    #include "textures/Karatel.hpp"
+};
