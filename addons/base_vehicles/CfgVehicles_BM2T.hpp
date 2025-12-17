187ffa71b10f6d9d57580b9d856365e2561ea75b
Patryk Kaniewski <p7tryk@gmail.com>
2025-12-18 00:37:09 +0100

    base_vehicles: add unarmed BM-2P

--- addons/base_vehicles/CfgVehicles_BM2T.hpp
+++ addons/base_vehicles/CfgVehicles_BM2T.hpp
@@ -491,6 +491,210 @@ class GCLASS(BM2_IFV2):  O_APC_Tracked_02_30mm_lxWS {
     };
 };
 
+class O_R_APC_Tracked_02_medical_F;
+class GCLASS(BM2_unarmed): O_R_APC_Tracked_02_medical_F {
+    displayName = "BM-2P (Unarmed)";
+    faction = QGCLASS(base_vehicles);
+    SCOPE_DLC_VEHICLE;
+    EMPTY_INVENTORY;
+    crew = "Civilian";
+    typicalCargo[] = {"Soldier"};
+    ace_refuel_fuelCapacity = 1200;
+    class TextureSources {
+        class ardistan {
+            displayName = "Ardistan (Green Net)";
+            IFV2_ARDISTAN(NET_GREEN)
+            factions[] = {};
+        };
+        class ardistan_desert {
+            displayName = "Ardistan (Desert Net)";
+            IFV2_ARDISTAN(NET_DESERT)
+            factions[] = {};
+        };
+        class ardistan_jungle {
+            displayName = "Ardistan (Jungle Net)";
+            IFV2_ARDISTAN(NET_JUNGLE)
+            factions[] = {};
+        };
+        class ardistan_winter {
+            displayName = "Ardistan (Winter Net)";
+            IFV2_ARDISTAN(NET_WINTER)
+            factions[] = {};
+        };
+        class Green {
+            displayName = "Green (Green Net)";
+            IFV2_GREEN(NET_GREEN)
+            factions[] = {};
+        };
+        class Green_desert {
+            displayName = "Green (Desert Net)";
+            IFV2_GREEN(NET_DESERT)
+            factions[] = {};
+        };
+        class Green_jungle {
+            displayName = "Green (Jungle Net)";
+            IFV2_GREEN(NET_JUNGLE)
+            factions[] = {};
+        };
+        class Green_winter {
+            displayName = "Green (Winter Net)";
+            IFV2_GREEN(NET_WINTER)
+            factions[] = {};
+        };
+        class GreenHex {
+            displayName = "Hex Green (Hex Green Net)";
+            IFV2_GREENHEX(NET_GREENHEX)
+            factions[] = {};
+        };
+        class GreenHex_desert {
+            displayName = "Hex Green (Hex Ochre Net)";
+            IFV2_GREENHEX(NET_HEX)
+            factions[] = {};
+        };
+        class GreenHex_winter {
+            displayName = "Hex Green (Winter Net)";
+            IFV2_GREENHEX(NET_WINTER)
+            factions[] = {};
+        };
+        class Hex {
+            displayName = "Hex Ochre (Hex Green Net)";
+            IFV2_HEX(NET_GREENHEX)
+            factions[] = {};
+        };
+        class Hex_desert {
+            displayName = "Hex Ochre (Hex Ochre Net)";
+            IFV2_HEX(NET_HEX)
+            factions[] = {};
+        };
+        class Hex_winter {
+            dislayName = "Hex Ochre (Winter Net)";
+            IFV2_HEX(NET_WINTER)
+            factions[] = {};
+        };
+        class Sep_01 {
+            displayName = "Separatist (Green Net)";
+            IFV2_SEP(NET_GREEN)
+            factions[] = {};
+        };
+        class Sep_01_desert {
+            displayName = "Separatist (Desert Net)";
+            IFV2_SEP(NET_DESERT)
+            factions[] = {};
+        };
+        class Sep_01_jungle {
+            displayName = "Separatist (Jungle Net)";
+            IFV2_SEP(NET_JUNGLE)
+            factions[] = {};
+        };
+        class Sep_01_winter {
+            displayName = "Separatist (Winter Net)";
+            IFV2_SEP(NET_WINTER)
+            factions[] = {};
+        };
+        class Grey {
+            displayName = "Grey (Green Net)";
+            IFV2_GREY(NET_GREEN)
+            factions[] = {};
+        };
+        class Grey_desert {
+            displayName = "Grey (Desert Net)";
+            IFV2_GREY(NET_DESERT)
+            factions[] = {};
+        };
+        class Grey_jungle {
+            displayName = "Grey (Jungle Net)";
+            IFV2_GREY(NET_JUNGLE)
+            factions[] = {};
+        };
+        class Grey_winter {
+            displayName = "Grey (Winter Net)";
+            IFV2_GREY(NET_WINTER)
+            factions[] = {};
+        };
+        class SFIA {
+            displayName = "Mudwalker (Desert Net)";
+            IFV2_SFIA(NET_DESERT)
+            factions[] = {};
+        };
+        class SFIA_green {
+            displayName = "Mudwalker (Green Net)";
+            IFV2_SFIA(NET_GREEN)
+            factions[] = {};
+        };
+        class SFIA_jungle {
+            displayName = "Mudwalker (Jungle Net)";
+            IFV2_SFIA(NET_JUNGLE)
+            factions[] = {};
+        };
+        class SFIA_winter {
+            displayName = "Mudwalker (Winter Net)";
+            IFV2_SFIA(NET_WINTER)
+            factions[] = {};
+        };
+        class Sand {
+            displayName = "Sand (Desert Net)";
+            IFV2_SAND(NET_DESERT)
+            factions[] = {};
+        };
+        class Sand_green {
+            displayName = "Sand (Green Net)";
+            IFV2_SAND(NET_GREEN)
+            factions[] = {};
+        };
+        class Sand_jungle {
+            displayName = "Sand (Jungle Net)";
+            IFV2_SAND(NET_JUNGLE)
+            factions[] = {};
+        };
+        class Sand_winter {
+            displayName = "Sand (Winter Net)";
+            IFV2_SAND(NET_WINTER)
+            factions[] = {};
+        };
+        class WoodlandHex {
+            displayName = "Hex Woodland (Desert Net)";
+            IFV2_WOODLANDHEX(NET_DESERT)
+            factions[] = {};
+        };
+        class WoodlandHex_green {
+            displayName = "Hex Woodland (Green Net)";
+            IFV2_WOODLANDHEX(NET_GREEN)
+            factions[] = {};
+        };
+        class WoodlandHex_jungle {
+            displayName = "Hex Woodland (Jungle Net)";
+            IFV2_WOODLANDHEX(NET_JUNGLE)
+            factions[] = {};
+        };
+        class WoodlandHex_winter {
+            displayName = "Hex Woodland (Winter Net)";
+            IFV2_WOODLANDHEX(NET_WINTER)
+            factions[] = {};
+        };
+        class Takistan {
+            displayName = "Hex Mixed (Desert Net)";
+            IFV2_TAKISTAN(NET_DESERT)
+            factions[] = {};
+        };
+        class Takistan_green {
+            displayName = "Hex Mixed (Green Net)";
+            IFV2_TAKISTAN(NET_GREEN)
+            factions[] = {};
+        };
+        class Takistan_jungle {
+            displayName = "Hex Mixed (Jungle Net)";
+            IFV2_TAKISTAN(NET_JUNGLE)
+            factions[] = {};
+        };
+        class Takistan_winter {
+            displayName = "Hex Mixed (Winter Net)";
+            IFV2_TAKISTAN(NET_WINTER)
+            factions[] = {};
+        };
+    };
+};
+
+
 class O_APC_Tracked_02_AA_F;
 class GCLASS(BM2_AA): O_APC_Tracked_02_AA_F {
     displayName = "BM-2PVO (ZSU-35)";
