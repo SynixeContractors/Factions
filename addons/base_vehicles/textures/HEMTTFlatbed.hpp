ca26a342e8505db6fffbe23f4ec9b6dcd3c248ff
Brett Mayson <brett@mayson.io>
2025-12-19 20:34:44 -0600

    files

---
+++ addons/base_vehicles/textures/HEMTTFlatbed.hpp
@@ -0,0 +1,27 @@
+class TextureSources {
+    class Blufor {
+        displayName = "Sand";
+        factions[] = {};
+        textures[] = {"\a3\soft_f_beta\truck_01\data\truck_01_ext_01_co.paa","\a3\soft_f_beta\truck_01\data\truck_01_ext_02_co.paa","\a3\Soft_F_Enoch\Truck_01\Data\truck_01_ammo_co.paa","\a3\Soft_F_Enoch\Truck_01\Data\Truck_01_cargo_CO.paa"};
+    };
+    class Olive {
+        displayName = "Olive";
+        textures[] = {"\a3\soft_f_Exp\truck_01\data\truck_01_ext_01_olive_co.paa","\a3\soft_f_Exp\truck_01\data\truck_01_ext_02_olive_co.paa","\a3\Soft_F_Enoch\Truck_01\Data\truck_01_ammo_pacific_co.paa","\a3\Soft_F_Enoch\Truck_01\Data\Truck_01_cargo_pacific_CO.paa"};
+        factions[] = {};
+    };
+    class France {
+        displayName = "Savanna";
+        textures[] = {"\A3_atlas\Soft_F_Atlas\Truck_01\Data\Truck_01_ext_01_fr_CO.paa","\A3_atlas\Soft_F_Atlas\Truck_01\Data\Truck_01_ext_02_fr_CO.paa","\A3_atlas\Soft_F_Atlas\Truck_01\Data\Truck_01_ammo_fr_CO.paa","\A3_atlas\Soft_F_Atlas\Truck_01\Data\Truck_01_cargo_fr_CO.paa"};
+        factions[] = {};
+    };
+    class Brown {
+        displayName = "Brown";
+        textures[] = {"\A3_Atlas\Soft_F_Atlas\Truck_01\Data\Truck_01_ext_01_brown_CO.paa","\A3_Atlas\Soft_F_Atlas\Truck_01\Data\Truck_01_ext_02_brown_CO.paa","\A3_Atlas\Soft_F_Atlas\Truck_01\Data\Truck_01_ammo_IDF_CO.paa","\A3_Atlas\Soft_F_Atlas\Truck_01\Data\Truck_01_cargo_IDF_CO.paa"};
+        factions[] = {};
+    };
+    class KZG {
+        displayName = "Khaybaran";
+        textures[] = {"\A3_Atlas\Soft_F_Atlas\Truck_01\Data\Truck_01_ext_01_kzg_CO.paa","\A3_Atlas\Soft_F_Atlas\Truck_01\Data\Truck_01_ext_02_kzg_CO.paa","\A3_Atlas\Soft_F_Atlas\Truck_01\Data\Truck_01_ammo_kzg_CO.paa","\A3_Atlas\Soft_F_Atlas\Truck_01\Data\Truck_01_cargo_ADF_CO.paa"};
+        factions[] = {};
+    };
+};
