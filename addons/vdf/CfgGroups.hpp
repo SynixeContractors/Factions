a20b97164e14041a80f2e230811cebc118d4681b
Brett Mayson <brett@mayson.io>
2025-12-21 00:19:33 -0600

    Viatken Defense Forces

---
+++ addons/vdf/CfgGroups.hpp
@@ -0,0 +1,10 @@
+class CfgGroups {
+    class INDEP {
+        class CLASS(t2_indep) {
+            name = "[T3] Viatken Defense Forces";
+            @Groups(T2,INDEP,Army_Combat);
+            @Groups(T2,INDEP,Army_Garrison);
+            @Groups(T2,INDEP,Army_Recon);
+        };
+    };
+};
