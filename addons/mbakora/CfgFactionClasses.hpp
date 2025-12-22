c143e55afe73041d8eec37a5a250b3147124c377
Brett Mayson <brett@mayson.io>
2025-12-21 19:52:02 -0600

    fix launch

--- addons/mbakora/CfgFactionClasses.hpp
+++ addons/mbakora/CfgFactionClasses.hpp
@@ -1,7 +1,7 @@
 class CfgFactionClasses {
     class CLASS(t3_opfor) {
         SCOPE_DLC;
-        displayName = FACTION_NAME
+        displayName = FACTION_NAME;
         priority = 1;
         side = 0; // OPFOR
     };
