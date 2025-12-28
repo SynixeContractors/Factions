fa1c42d43ae9d470004593274d97cda851dac789
Patryk Kaniewski <p7tryk@gmail.com>
2025-12-28 05:26:43 +0100

    base_vehicles: su75 stealth

--- addons/base_vehicles/CfgVehicles_SU75.hpp
+++ addons/base_vehicles/CfgVehicles_SU75.hpp
@@ -1,5 +1,4 @@
 class O_Plane_Fighter_02_F;
-class Components;
 class GCLASS(SU75): O_Plane_Fighter_02_F {
     displayName = "SU-75 (External Stores)";
     faction = QGCLASS(base_vehicles);
@@ -9,7 +8,7 @@ class GCLASS(SU75): O_Plane_Fighter_02_F {
     typicalCargo[] = {"Soldier"};
     ace_refuel_fuelCapacity = 10300;
     #include "textures/SU75.hpp"
-    class Components: Components {
+    class Components {
       class TransportPylonsComponent {
         #include "pylons/SU75.hpp"
       };
@@ -25,26 +24,9 @@ class GCLASS(SU75_Stealth): O_Plane_Fighter_02_Stealth_F {
     typicalCargo[] = {"Soldier"};
     ace_refuel_fuelCapacity = 10300;
     #include "textures/SU75.hpp"
-    class Components: Components {
+    class Components {
       class TransportPylonsComponent {
-        #include "pylons/SU75.hpp"
+        #include "pylons/SU75_Stealth.hpp"
       };
     };
 };
-
-// hardpoints[] = {"B_BIM9X_RAIL",    "B_BIM9X_DUAL_RAIL",
-//                 "B_AMRAAM_D_RAIL", "B_AMRAAM_D_DUAL_RAIL",
-//                 "B_AGM65_RAIL",    "B_AGM65_DUAL_RAIL",
-//                 "B_GBU12",         "B_GBU12_DUAL_RAIL",
-//                 "B_HARM_RAIL",     "B_SDB_QUAD_RAIL"};
-
-// hardpoints[] = {"B_AMRAAM_D_INT", "B_SDB_QUAD_RAIL"};
-
-// hardpoints[] = {"B_MISSILE_PYLON","B_BOMB_PYLON"};
-// hardpoints[] = {"O_MISSILE_PYLON","UNI_SCALPEL"};
-
-// pylonWeapon = "ace_missile_aim120_r77Launcher";
-// pylonWeapon = "ace_missile_aim9_r74";
-// hardpoints[] = {"B_AGM_154"};
-// hardpoints[] = {"B_GBU12_DUAL_RAIL","I_GBU12_DUAL_RAIL"};
-//hardpoints[] = {"O_MISSILE_PYLON","O_VIKHR"};
--- addons/base_vehicles/pylons/SU75_Stealh.hpp
+++
@@ -1,2 +0,0 @@
-class SU75_Stealth_pylons: SU75_pylons {
-};
