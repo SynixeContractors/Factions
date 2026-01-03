4f76eb843ceb7b34233e17136e3bd4fbab3c2754
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-03 09:06:06 +0100

    khaybara: stub

---
+++ addons/khaybara/CfgGroups.hpp
@@ -0,0 +1,8 @@
+class CfgGroups {
+    class BluFor {
+        class CLASS(t2_blufor) {
+            name = "[T2] Khaybaran Confederation";
+            @Groups(T2,BLUFOR,Army_Combat);
+        };
+    };
+};
