8440c07f2d1b14475bf30883035e2bc94af49538
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-04 00:27:42 +0100

    khabaran: include (l)mg

--- addons/khaybara/army_combat/CfgVehicles.hpp
+++ addons/khaybara/army_combat/CfgVehicles.hpp
@@ -73,24 +73,12 @@ class CfgVehicles {
     };
     class CLASS(Autorifleman): CLASS(RiflemanBackpack) {
         @Role(Autorifleman);
-        @Primary({
-            "weapons": {
-                "Aegis_arifle_M16A4_F": {
-                    "probability": 1,
-                    "magazinesVest": {
-                        "30Rnd_556x45_Stanag_Sand": 8,
-                    },
-                },
-            },
-            "optics": {
-                "Aegis_optic_1p87_snd": 0.4,
-                "optic_VRCO_tan_RF": 0.6,
-            },
-        });
+        #include "..\weapons\mg.hpp"
         #include "..\weapons\pistol.hpp"
     };
     class CLASS(Machinegunner): CLASS(Rifleman) {
         @Role(Machinegunner);
+        #include "..\weapons\mg.hpp"
     };
     class CLASS(AsstMachinegunner): CLASS(RiflemanBackpack) {
         @Role(AsstMachinegunner);
