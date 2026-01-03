aa7400a3531c3d5bdb9d34aa5ea81b6b5b715569
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-04 00:27:42 +0100

    khabaran: include (l)mg

---
+++ addons/khaybara/weapons/lmg.hpp
@@ -0,0 +1,14 @@
+        @Primary({
+            "weapons": {
+                "Aegis_arifle_M16A4_F": {
+                    "probability": 1,
+                    "magazinesVest": {
+                        "30Rnd_556x45_Stanag_Sand": 8,
+                    },
+                },
+            },
+            "optics": {
+                "Aegis_optic_1p87_snd": 0.4,
+                "optic_VRCO_tan_RF": 0.6,
+            },
+        });
