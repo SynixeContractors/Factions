38efc55fedacf9935bbd805812aa00f3e69706c1
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-04 00:24:49 +0100

    khaybara: gl

---
+++ addons/khaybara/weapons/gl.hpp
@@ -0,0 +1,15 @@
+        @Primary({
+            "weapons": {
+                "Aegis_arifle_M4A1_GL_sand_F": {
+                    "probability": 1,
+                    "magazinesVest": {
+                        "30Rnd_556x45_Stanag_Sand": 8,
+                        "1Rnd_HE_Grenade_shell": 4,
+                    },
+                },
+            },
+            "optics": {
+                "Aegis_optic_1p87_snd": 0.4,
+                "optic_VRCO_tan_RF": 0.6,
+            },
+        });
