17955e1548b8549a0b79d3abd3c39f3c96f4cd1a
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-04 19:13:23 +0100

    khaybara: fix armor

--- addons/khaybara/army_combat/CfgVehicles.hpp
+++ addons/khaybara/army_combat/CfgVehicles.hpp
@@ -46,8 +46,9 @@ class CfgVehicles {
 
         @Vests({
             "variants": {
-                "V_HarnessO_brn": 0.5,
-                "V_HarnessOSpec_brn": 0.5,
+                "Aegis_V_CarrierRigKBT_01_cqb_khk_F": 0.2,
+                "Aegis_V_CarrierRigKBT_01_tac_khk_F": 0.5,
+                "Aegis_V_CarrierRigKBT_01_recon_khk_F": 0.3,
             },
             "packs": [
                 "t2_standard",
