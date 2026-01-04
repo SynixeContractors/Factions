ef7638a23805cf129cb906c2323c68a2544696de
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-04 03:02:44 +0100

    base_vehicles: templates for polaris,typhoon,t100,t14

---
+++ templates/cars/Polaris.hpp
@@ -0,0 +1,36 @@
+class GCLASS(Polaris);
+class CLASS(Polaris): GCLASS(Polaris) {
+    SCOPE_DLC;
+    faction = TEMPLATE_FACTION;
+    side = TEMPLATE_SIDE;
+    crew = QCLASS(Rifleman);
+    typicalCargo[] = {QCLASS(Rifleman)};
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+};
+class GCLASS(Polaris_NoDoor);
+class CLASS(Polaris_NoDoor): GCLASS(Polaris_NoDoor) {
+    SCOPE_DLC;
+    faction = TEMPLATE_FACTION;
+    side = TEMPLATE_SIDE;
+    crew = QCLASS(Rifleman);
+    typicalCargo[] = {QCLASS(Rifleman)};
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+};
+class GCLASS(Polaris_Titan);
+class CLASS(Polaris_Titan): GCLASS(Polaris_Titan) {
+    SCOPE_DLC;
+    faction = TEMPLATE_FACTION;
+    side = TEMPLATE_SIDE;
+    crew = QCLASS(Rifleman);
+    typicalCargo[] = {QCLASS(Rifleman)};
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+};
+class GCLASS(Polaris_HMG);
+class CLASS(Polaris_HMG): GCLASS(Polaris_HMG) {
+    SCOPE_DLC;
+    faction = TEMPLATE_FACTION;
+    side = TEMPLATE_SIDE;
+    crew = QCLASS(Rifleman);
+    typicalCargo[] = {QCLASS(Rifleman)};
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+};
---
+++ templates/tanks/T100_Railgun.hpp
@@ -0,0 +1,9 @@
+class GCLASS(T100_Railgun);
+class CLASS(T100_Railgun): GCLASS(T100_Railgun) {
+    SCOPE_DLC;
+    faction = TEMPLATE_FACTION;
+    side = TEMPLATE_SIDE;
+    crew = QCLASS(Crewman);
+    typicalCargo[] = {QCLASS(Crewman)};
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+};
