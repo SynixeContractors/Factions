5fe65e69a15fd2014b3b5a6fcd2cccfe0ff43eda
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-04 01:39:19 +0100

    templates: btr templates

---
+++ templates/apc/BTR_IFV.hpp
@@ -0,0 +1,9 @@
+class GCLASS(BTR_IFV);
+class CLASS(BTR_IFV): GCLASS(BTR_IFV) {
+    SCOPE_DLC;
+    faction = TEMPLATE_FACTION;
+    side = TEMPLATE_SIDE;
+    crew = QCLASS(Crewman);
+    typicalCargo[] = {QCLASS(Crewman)};
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+};
