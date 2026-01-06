e2a0217f60a41a791d1214b21a2f2ee7cb8e7d1b
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-06 13:27:28 +0100

    khaybara: Special forces

---
+++ addons/khaybara/wear/uniform_sof.hpp
@@ -0,0 +1,13 @@
+@Uniforms({
+  "variants" : {
+    "Atlas_U_B_K_CombatUniform" : 2,
+    "Atlas_U_B_K_CombatUniform_shortsleeve" : 3,
+    "U_lxWS_B_CombatUniform_desert_tshirt" : 1,
+    "synixe_mgp_g3_field_set_khk_khk" : 1,
+    "synixe_mgp_g3_tshirt_tan_khk" : 1,
+    "synixe_mgp_g3_field_set_atacsau_khk" : 1,
+  },
+  "packs" : [
+    "rifleman_medical",
+  ],
+});
---
+++ addons/khaybara/wear/vest_sof.hpp
@@ -0,0 +1,10 @@
+@Vests({
+  "variants" : {
+    "synixe_mgp_vest_mmac_assaulter_cb" : 0.33,
+    "synixe_mgp_vest_mmac_light_cb" : 0.33,
+    "synixe_mgp_vest_mmac_teamleader_cb" : 0.33,
+  },
+  "packs" : [
+    "t2_standard",
+  ],
+});
