54aedeceedc9ba21ee7d97b1c76c65204628e48b
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-06 14:06:28 +0100

    khaybara: attachmenst for army

--- addons/khaybara/weapons/attachments_army.hpp
+++ addons/khaybara/weapons/attachments_army.hpp
@@ -4,13 +4,7 @@
     "" : 2,
   },
   "optics": {
-    "sma_spitfire_03_rds_low_black": 1,
-    "SMA_MICRO_T2_3XDOWN": 1,
-    "optic_MRCO": 1,
-    "SMA_eotechG33_tan_3XDOWN": 1,
-    "SMA_eotech_T": 1,
-    "SMA_ELCAN_SPECTER_TAN_ARDRDS": 1,
-    "optic_DMS": 1,
-    "optic_r1_high_sand_lxWS": 1,
+    "": 0.2,
+    "sma_spitfire_01_black": 0.8,
   },
 });
