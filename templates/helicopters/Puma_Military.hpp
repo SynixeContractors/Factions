1b22eea8529d34ecc51487b81de818ec9be1e314
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-07 19:33:52 +0100

    templates: puma and wildcat

---
+++ templates/helicopters/Puma_Military.hpp
@@ -0,0 +1,104 @@
+class GCLASS(Puma);
+class CLASS(Puma): GCLASS(Puma) {
+    SCOPE_DLC;
+    faction = TEMPLATE_FACTION;
+    side = TEMPLATE_SIDE;
+    crew = QCLASS(HeliPilot);
+    typicalCargo[] = {QCLASS(HeliPilot)};
+    #ifdef TEMPLATE_CAMO_LIST
+    textureList[] = TEMPLATE_CAMO_LIST;
+    #else
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+    #endif
+};
+class GCLASS(PumaA);
+class CLASS(PumaA): GCLASS(PumaA) {
+    SCOPE_DLC;
+    faction = TEMPLATE_FACTION;
+    side = TEMPLATE_SIDE;
+    crew = QCLASS(HeliPilot);
+    typicalCargo[] = {QCLASS(HeliPilot)};
+    #ifdef TEMPLATE_CAMO_LIST
+    textureList[] = TEMPLATE_CAMO_LIST;
+    #else
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+    #endif
+};
+class GCLASS(PumaM);
+class CLASS(PumaM): GCLASS(PumaM) {
+    SCOPE_DLC;
+    faction = TEMPLATE_FACTION;
+    side = TEMPLATE_SIDE;
+    crew = QCLASS(HeliPilot);
+    typicalCargo[] = {QCLASS(HeliPilot)};
+    #ifdef TEMPLATE_CAMO_LIST
+    textureList[] = TEMPLATE_CAMO_LIST;
+    #else
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+    #endif
+};
+class GCLASS(PumaT);
+class CLASS(PumaT): GCLASS(PumaT) {
+    SCOPE_DLC;
+    faction = TEMPLATE_FACTION;
+    side = TEMPLATE_SIDE;
+    crew = QCLASS(HeliPilot);
+    typicalCargo[] = {QCLASS(HeliPilot)};
+    #ifdef TEMPLATE_CAMO_LIST
+    textureList[] = TEMPLATE_CAMO_LIST;
+    #else
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+    #endif
+};
+class GCLASS(Cougar);
+class CLASS(Cougar): GCLASS(Cougar) {
+    SCOPE_DLC;
+    faction = TEMPLATE_FACTION;
+    side = TEMPLATE_SIDE;
+    crew = QCLASS(HeliPilot);
+    typicalCargo[] = {QCLASS(HeliPilot)};
+    #ifdef TEMPLATE_CAMO_LIST
+    textureList[] = TEMPLATE_CAMO_LIST;
+    #else
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+    #endif
+};
+class GCLASS(Cougar_Armed);
+class CLASS(Cougar_Armed): GCLASS(Cougar_Armed) {
+    SCOPE_DLC;
+    faction = TEMPLATE_FACTION;
+    side = TEMPLATE_SIDE;
+    crew = QCLASS(HeliPilot);
+    typicalCargo[] = {QCLASS(HeliPilot)};
+    #ifdef TEMPLATE_CAMO_LIST
+    textureList[] = TEMPLATE_CAMO_LIST;
+    #else
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+    #endif
+};
+class GCLASS(Cougar_Pylon);
+class CLASS(Cougar_Pylon): GCLASS(Cougar_Pylon) {
+    SCOPE_DLC;
+    faction = TEMPLATE_FACTION;
+    side = TEMPLATE_SIDE;
+    crew = QCLASS(HeliPilot);
+    typicalCargo[] = {QCLASS(HeliPilot)};
+    #ifdef TEMPLATE_CAMO_LIST
+    textureList[] = TEMPLATE_CAMO_LIST;
+    #else
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+    #endif
+};
+class GCLASS(Cougar_Rescue);
+class CLASS(Cougar_Rescue): GCLASS(Cougar_Rescue) {
+    SCOPE_DLC;
+    faction = TEMPLATE_FACTION;
+    side = TEMPLATE_SIDE;
+    crew = QCLASS(HeliPilot);
+    typicalCargo[] = {QCLASS(HeliPilot)};
+    #ifdef TEMPLATE_CAMO_LIST
+    textureList[] = TEMPLATE_CAMO_LIST;
+    #else
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+    #endif
+};
---
+++ templates/helicopters/Wildcat.hpp
@@ -0,0 +1,26 @@
+class GCLASS(Wildcat);
+class CLASS(Wildcat): GCLASS(Wildcat) {
+    SCOPE_DLC;
+    faction = TEMPLATE_FACTION;
+    side = TEMPLATE_SIDE;
+    crew = QCLASS(HeliPilot);
+    typicalCargo[] = {QCLASS(HeliPilot)};
+    #ifdef TEMPLATE_CAMO_LIST
+    textureList[] = TEMPLATE_CAMO_LIST;
+    #else
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+    #endif
+};
+class GCLASS(Wildcat_Pylons);
+class CLASS(Wildcat_Pylons): GCLASS(Wildcat_Pylons) {
+    SCOPE_DLC;
+    faction = TEMPLATE_FACTION;
+    side = TEMPLATE_SIDE;
+    crew = QCLASS(HeliPilot);
+    typicalCargo[] = {QCLASS(HeliPilot)};
+    #ifdef TEMPLATE_CAMO_LIST
+    textureList[] = TEMPLATE_CAMO_LIST;
+    #else
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+    #endif
+};
