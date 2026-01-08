015b335e8c1cb033b194ce2d289b778dc2755411
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-08 13:22:32 +0100

    base_vehicles: move BM2AA texture configs

---
+++ addons/base_vehicles/textures/BM2_AA.hpp
@@ -0,0 +1,79 @@
+class TextureSources {
+    class Hex {
+        displayName = "Hex Ochre";
+        textures[] = {
+            "A3\Armor_F_Beta\APC_Tracked_02\Data\apc_tracked_02_ext_01_aa_hexarid_co.paa",
+            "A3\Armor_F_Beta\APC_Tracked_02\Data\apc_tracked_02_ext_02_hexarid_co.paa",
+            "A3\Armor_F_Beta\APC_Tracked_01\Data\apc_tracked_01_aa_tower_opfor_co.paa",
+            "A3\Armor_F\Data\camonet_CSAT_HEX_Desert_CO.paa",
+            "A3\armor_f\data\cage_csat_co.paa"
+        };
+        factions[] = {};
+    };
+    class GreenHex {
+        displayName = "Hex Green";
+        textures[] = {
+            "A3\Armor_F_Exp\APC_Tracked_02\Data\APC_Tracked_02_ext_01_AA_ghex_CO.paa",
+            "A3\Armor_F_Exp\APC_Tracked_02\Data\APC_Tracked_02_ext_02_ghex_CO.paa",
+            "A3\Armor_F_Exp\APC_Tracked_02\Data\APC_Tracked_01_AA_Tower_ghex_CO.paa",
+            "A3\Armor_F\Data\camonet_CSAT_HEX_Green_CO.paa",
+            "A3\armor_f\data\cage_csat_green_CO.paa"
+        };
+        factions[] = {};
+    };
+    class Green {
+        displayName = "Green";
+        textures[] = {
+            "\A3_Aegis\Armor_F_Aegis\APC_Tracked_02\Data\APC_Tracked_02_ext_01_AA_RUkhk_CO.paa",
+            "\A3_Aegis\Armor_F_Aegis\APC_Tracked_02\Data\APC_Tracked_02_ext_02_RUkhk_CO.paa",
+            "\A3_Aegis\Armor_F_Aegis\APC_Tracked_02\Data\APC_Tracked_01_AA_Tower_RUkhk_CO.paa",
+            "\A3_Aegis\Armor_F_Aegis\Data\camonet_RUS_green_CO.paa",
+            "\A3_Aegis\Armor_F_Aegis\Data\cage_RUkhk_CO.paa"
+        };
+        factions[] = {};
+    };
+    class Sep_01 {
+        displayName = "Separatists";
+        textures[] = {
+            "\A3_Opf\Armor_F_Opf\APC_Tracked_02\Data\APC_Tracked_02_ext_01_Opf_CO.paa",
+            "\A3_Aegis\Armor_F_Aegis\APC_Tracked_02\Data\APC_Tracked_02_ext_02_RUkhk_CO.paa",
+            "\A3_Aegis\Armor_F_Aegis\APC_Tracked_02\Data\APC_Tracked_01_AA_Tower_RUkhk_CO.paa",
+            "\A3_Aegis\Armor_F_Aegis\Data\camonet_RUS_green_CO.paa",
+            "\A3_Aegis\Armor_F_Aegis\Data\cage_RUkhk_CO.paa"
+        };
+        factions[] = {};
+    };
+    class SFIA {
+        displayName = "Mudwalker";
+        textures[] = {
+            "lxws\vehicles_f_lxws\data\APC_Tracked_02\kamysh_aa_africa1_co.paa",
+            "lxws\vehicles_f_lxws\data\APC_Tracked_02\kamysh_africa2_co.paa",
+            "lxws\vehicles_f_lxws\data\APC_Tracked_02\kamysh_aa_africa3_co.paa",
+            "A3\Armor_F\Data\camonet_CSAT_HEX_Desert_CO.paa",
+            "A3\armor_f\data\cage_csat_co.paa"
+        };
+        factions[] = {};
+    };
+    class Sand {
+        displayName = "Sand";
+        textures[] = {
+            "lxws\vehicles_f_lxws\data\APC_Tracked_02\kamysh_aa_sand1_co.paa",
+            "lxws\vehicles_f_lxws\data\APC_Tracked_02\kamysh_sand2_co.paa",
+            "lxws\vehicles_f_lxws\data\APC_Tracked_02\kamysh_aa_sand3.paa",
+            "A3\Armor_F\Data\camonet_CSAT_HEX_Desert_CO.paa",
+            "A3\armor_f\data\cage_csat_co.paa"
+        };
+        factions[] = {};
+    };
+    class cdf {
+        displayName = "CDF";
+        textures[] = {
+            "\A3_Atlas\Armor_F_Atlas\APC_Tracked_02\Data\APC_Tracked_02_ext_01_CDF_CO.paa",
+            "\A3_Atlas\Armor_F_Atlas\APC_Tracked_02\Data\APC_Tracked_02_ext_02_CDF_CO.paa",
+            "\A3_Aegis\Armor_F_Aegis\APC_Tracked_02\Data\APC_Tracked_01_AA_Tower_CDF_CO",
+            NET_GREEN,
+            "\A3_Atlas\Armor_F_Atlas\Data\cage_CDF_CO.paa"
+        };
+        factions[] = {};
+    };
+};
