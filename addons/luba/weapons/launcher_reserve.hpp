73221cc95b85ee86f59458e6a0b383dc06d051ee
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-09 00:38:23 +0100

    luba: fix rpg

--- addons/luba/weapons/launcher_reserve.hpp
+++ addons/luba/weapons/launcher_reserve.hpp
@@ -3,7 +3,7 @@
         "": 0.95,
         "launch_RPG7_F": {
             "probability": 0.03,
-            "loadedPrimary": "launch_RPG7_F",
+            "loadedPrimary": "RPG7_F",
         },
         "Aegis_launch_RPG7M_F": {
             "probability": 0.02,
