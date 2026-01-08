caf11da7dbadc05c80a4ace435d92bfbc3ca63e7
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-08 13:29:49 +0100

    base_vehicles: BM2 move texture configs

---
+++ addons/base_vehicles/textures/BM2_IFV1.hpp
@@ -0,0 +1,147 @@
+class TextureSources {
+    class Green {
+        displayName = "Green (Green Net)";
+        IFV1_GREEN(NET_GREEN)
+        factions[] = {};
+    };
+    class Green_desert {
+        displayName = "Green (Desert Net)";
+        IFV1_GREEN(NET_DESERT)
+        factions[] = {};
+    };
+    class Green_jungle {
+        displayName = "Green (Jungle Net)";
+        IFV1_GREEN(NET_JUNGLE)
+        factions[] = {};
+    };
+    class Green_winter {
+        displayName = "Green (Winter Net)";
+        IFV1_GREEN(NET_WINTER)
+        factions[] = {};
+    };
+    class GreenHex {
+        displayName = "Hex Green (Hex Green Net)";
+        IFV1_GREENHEX(NET_GREENHEX)
+        factions[] = {};
+    };
+    class GreenHex_desert {
+        displayName = "Hex Green (Hex Ochre Net)";
+        IFV1_GREENHEX(NET_HEX)
+        factions[] = {};
+    };
+    class GreenHex_winter {
+        displayName = "Hex Green (Winter Net)";
+        IFV1_GREENHEX(NET_WINTER)
+        factions[] = {};
+    };
+    class Hex {
+        displayName = "Hex Ochre (Hex Green Net)";
+        IFV1_HEX(NET_GREENHEX)
+        factions[] = {};
+    };
+    class Hex_desert {
+        displayName = "Hex Ochre (Hex Ochre Net)";
+        IFV1_HEX(NET_HEX)
+        factions[] = {};
+    };
+    class Hex_winter {
+        displayName = "Hex Ochre (Winter Net)";
+        IFV1_HEX(NET_WINTER)
+        factions[] = {};
+    };
+    class Sand {
+        displayName = "Sand (Desert Net)";
+        IFV1_SAND(NET_DESERT)
+        factions[] = {};
+    };
+    class Sand_green {
+        displayName = "Sand (Green Net)";
+        IFV1_SAND(NET_GREEN)
+        factions[] = {};
+    };
+    class Sand_jungle {
+        displayName = "Sand (Jungle Net)";
+        IFV1_SAND(NET_JUNGLE)
+        factions[] = {};
+    };
+    class Sand_winter {
+        displayName = "Sand (Winter Net)";
+        IFV1_SAND(NET_WINTER)
+        factions[] = {};
+    };
+    class Sep_01 {
+        displayName = "Separatist (Green Net)";
+        IFV1_SEP(NET_GREEN)
+        factions[] = {};
+    };
+    class Sep_01_desert {
+        displayName = "Separatist (Desert Net)";
+        IFV1_SEP(NET_DESERT)
+        factions[] = {};
+    };
+    class Sep_01_jungle {
+        displayName = "Separatist (Jungle Net)";
+        IFV1_SEP(NET_JUNGLE)
+        factions[] = {};
+    };
+    class Sep_01_winter {
+        displayName = "Separatist (Winter Net)";
+        IFV1_SEP(NET_WINTER)
+        factions[] = {};
+    };
+    class SFIA {
+        displayName = "Mudwalker (Desert Net)";
+        IFV1_SFIA(NET_DESERT)
+        factions[] = {};
+    };
+    class SFIA_green {
+        displayName = "Mudwalker (Green Net)";
+        IFV1_SFIA(NET_GREEN)
+        factions[] = {};
+    };
+    class SFIA_jungle {
+        displayName = "Mudwalker (Jungle Net)";
+        IFV1_SFIA(NET_JUNGLE)
+        factions[] = {};
+    };
+    class SFIA_winter {
+        displayName = "Mudwalker (Winter Net)";
+        IFV1_SFIA(NET_WINTER)
+        factions[] = {};
+    };
+    class Takistan {
+        displayName = "Hex Mixed (Hex Green Net)";
+        IFV1_TAKISTAN(NET_GREENHEX)
+        factions[] = {};
+    };
+    class Takistan_desert {
+        displayName = "Hex Mixed (Hex Ochre Net)";
+        IFV1_TAKISTAN(NET_HEX)
+        factions[] = {};
+    };
+    class Takistan_winter {
+        displayName = "Hex Mixed (Winter Net)";
+        IFV1_TAKISTAN(NET_WINTER)
+        factions[] = {};
+    };
+    class cdf {
+        displayName = "CDF (Desert Net)";
+        IFV1_CDF(NET_DESERT)
+        factions[] = {};
+    };
+    class cdf_green {
+        displayName = "CDF (Green Net)";
+        IFV1_CDF(NET_GREEN)
+        factions[] = {};
+    };
+    class cdf_jungle {
+        displayName = "CDF (Jungle Net)";
+        IFV1_CDF(NET_JUNGLE)
+        factions[] = {};
+    };
+    class cdf_winter {
+        displayName = "CDF (Winter Net)";
+        IFV1_CDF(NET_WINTER)
+        factions[] = {};
+    };
+};
