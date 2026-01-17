c1484c3fc4e5be791fa06c63526af4050d22152f
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-17 08:51:38 +0100

    civilians: police stub

---
+++ addons/civilians/weapons/rifle.hpp
@@ -0,0 +1,22 @@
+@Primary({
+    "weapons": {
+        "Aegis_arifle_M4A1_F": {
+            "probability": 1,
+            "magazinesVest": {
+                "30Rnd_556x45_Stanag_green": 4,
+            },
+        },
+        "Aegis_arifle_M4A1_short_F": {
+            "probability": 1,
+            "magazinesVest": {
+                "30Rnd_556x45_Stanag_green": 4,
+            },
+        },
+        "Aegis_arifle_M4A1_grip_F": {
+            "probability": 1,
+            "magazinesVest": {
+                "30Rnd_556x45_Stanag_green": 4,
+            },
+        },
+    },
+});
