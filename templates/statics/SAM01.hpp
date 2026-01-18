229c18bd67535790dd4619270755be75febec9c1
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-18 11:27:25 +0100

    templates: fix sam launchers template

--- templates/statics/SAM01.hpp
+++ templates/statics/SAM01.hpp
@@ -3,10 +3,21 @@ class CLASS(Radar01): GCLASS(Radar01) {
     SCOPE_DLC;
     faction = TEMPLATE_FACTION;
     side = TEMPLATE_SIDE;
+    #ifdef TEMPLATE_CAMO_LIST
+    textureList[] = TEMPLATE_CAMO_LIST;
+    #else
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+    #endif
+
 };
 class GCLASS(SAM01);
 class CLASS(SAM01): GCLASS(SAM01) {
     SCOPE_DLC;
     faction = TEMPLATE_FACTION;
     side = TEMPLATE_SIDE;
+    #ifdef TEMPLATE_CAMO_LIST
+    textureList[] = TEMPLATE_CAMO_LIST;
+    #else
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+    #endif
 };
--- templates/statics/SAM02.hpp
+++ templates/statics/SAM02.hpp
@@ -3,10 +3,22 @@ class CLASS(Radar02): GCLASS(Radar02) {
     SCOPE_DLC;
     faction = TEMPLATE_FACTION;
     side = TEMPLATE_SIDE;
+    #ifdef TEMPLATE_CAMO_LIST
+    textureList[] = TEMPLATE_CAMO_LIST;
+    #else
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+    #endif
+
 };
 class GCLASS(SAM02);
 class CLASS(SAM02): GCLASS(SAM02) {
     SCOPE_DLC;
     faction = TEMPLATE_FACTION;
     side = TEMPLATE_SIDE;
+    #ifdef TEMPLATE_CAMO_LIST
+    textureList[] = TEMPLATE_CAMO_LIST;
+    #else
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+    #endif
+
 };
