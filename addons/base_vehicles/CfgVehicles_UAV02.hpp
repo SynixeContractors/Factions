26caf0296aa815edb6f8d932bf54ddab5b61a33d
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-25 06:15:26 +0100

    base_vehicles: add shahpar event camo (#145)

--- addons/base_vehicles/CfgVehicles_UAV02.hpp
+++ addons/base_vehicles/CfgVehicles_UAV02.hpp
@@ -1,4 +1,4 @@
-class O_UAV_02_dynamicLoadout_F;
+ADD_CAMO_EVENT_HANDLER(O_UAV_02_dynamicLoadout_F,UAV_02_dynamicLoadout_base_F);
 class GCLASS(Shahpar): O_UAV_02_dynamicLoadout_F {
     displayName = "Shahpar IV";
     faction = QGCLASS(base_vehicles);
@@ -6,6 +6,7 @@ class GCLASS(Shahpar): O_UAV_02_dynamicLoadout_F {
     EMPTY_INVENTORY;
     crew = "C_UAV_AI_F";
     ace_refuel_fuelCapacity = 480; //like mq1 predator
+    #include "eventhandler_camo.hpp"
     #include "textures/UAV02.hpp"
     // class Components {
     //   class TransportPylonsComponent {
