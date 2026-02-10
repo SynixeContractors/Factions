a94b28bdda48396716bec0347f7dc461086f5803
Patryk Kaniewski <p7tryk@gmail.com>
2026-02-10 18:07:37 +0100

    base_vehicles: jeep randomization

--- addons/base_vehicles/CfgVehicles_Jeep.hpp
+++ addons/base_vehicles/CfgVehicles_Jeep.hpp
@@ -1,9 +1,21 @@
+#define JEEP_TEXTURE_LIST textureList[] = {\
+  "Black",1,\
+  "Blue",1,\
+  "Brown",1,\
+  "Green",1,\
+  "Olive",1,\
+  "Orange",1,\
+  "Red",1,\
+  "White",1,\
+}
+
 class Offroad_02_base_F;
 class GCLASS(Jeep): Offroad_02_base_F {
     faction = QGCLASS(base_vehicles);
     SCOPE_DLC_VEHICLE;
     EMPTY_INVENTORY;
     ace_refuel_fuelCapacity = 80;
+    JEEP_TEXTURE_LIST;
     #include "textures\Jeep.hpp"
 };
 
@@ -13,5 +25,6 @@ class GCLASS(Jeep_LMG): Offroad_02_LMG_base_F {
     SCOPE_DLC_VEHICLE;
     EMPTY_INVENTORY;
     ace_refuel_fuelCapacity = 80;
+    JEEP_TEXTURE_LIST;
     #include "textures\Jeep.hpp"
 };
