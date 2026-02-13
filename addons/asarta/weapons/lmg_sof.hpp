e4f4770ea30290afdbde57c995dc4ec0edd3d3a0
Patryk Kaniewski <p7tryk@gmail.com>
2026-02-13 11:17:16 +0100

    asarta: stub

---
+++ addons/asarta/weapons/lmg_sof.hpp
@@ -0,0 +1,17 @@
+@Primary({
+    "weapons": {
+        "synixe_armoury_arifle_RPK03_sand": {
+            "probability": 1,
+            "magazinesVest": {
+                "100Rnd_580x42_Mag_F": 1,
+            },
+            "magazinesBackpack": {
+                "100Rnd_580x42_Mag_F": 3,
+            },
+        },
+    },
+    "optics": {
+        "Aegis_optic_1p87": 0.8,
+        "": 0.2,
+    },
+});
---
+++ addons/asarta/wear/helmet.hpp
@@ -0,0 +1,3 @@
+@Headgear({
+    "Atlas_H_PASGT_Cover_O_SAHex_F": 1,
+});
