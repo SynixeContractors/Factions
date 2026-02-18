7d7f0b9f5759a9676fc8fed8f198002eeafa51d6
Patryk Kaniewski <p7tryk@gmail.com>
2026-02-18 21:45:30 +0100

    aaf: add nlaw

--- addons/altis/weapons/launcher.hpp
+++ addons/altis/weapons/launcher.hpp
@@ -1,8 +1,12 @@
 @Launchers({
     "weapons": {
         "": 0.9,
+        "launch_NLAW_F": {
+            "probability": 0.07,
+            "loadedPrimary": "NLAW_F",
+        },
         "launch_PSRL1_digi_RF": {
-            "probability": 0.1,
+            "probability": 0.03,
             "loadedPrimary": "PSRL1_AT_RF",
         },
     },
