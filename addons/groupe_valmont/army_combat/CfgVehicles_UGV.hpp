5f46104259cf6ee2b019d0b8aacff7179d450cfc
Patryk Kaniewski <p7tryk@gmail.com>
2026-02-18 22:40:31 +0100

    fix grouppe valmont ugv

--- addons/groupe_valmont/army_combat/CfgVehicles_UGV.hpp
+++ addons/groupe_valmont/army_combat/CfgVehicles_UGV.hpp
@@ -5,8 +5,6 @@ class CLASS2(SIDE,UGV_RCWS): GCLASS(UGV_RCWS) {
     textureList[] = {"Green",1};
     editorPreview = "\A3\EditorPreviews_F_Exp\Data\CfgVehicles\B_T_UGV_01_rcws_olive_F.jpg";
     hiddenSelectionsTextures[] = {"\A3\Soft_F_Exp\UGV_01\Data\UGV_01_ext_olive_CO.paa","\A3\Soft_F_Exp\UGV_01\Data\UGV_01_int_olive_CO.paa","\A3\Data_F_Exp\Vehicles\Turret_olive_CO.paa"};
-    crew = QCLASS2(SIDE,Base);
-    typicalCargo[] = {};
 };
 
 class CLASS2(SIDE,UGV): GCLASS(UGV) {
@@ -16,7 +14,4 @@ class CLASS2(SIDE,UGV): GCLASS(UGV) {
     textureList[] = {"Green",1};
     editorPreview = "\A3\EditorPreviews_F_Exp\Data\CfgVehicles\B_T_UGV_01_olive_F.jpg";
     hiddenSelectionsTextures[] = {"\A3\Soft_F_Exp\UGV_01\Data\UGV_01_ext_olive_CO.paa","\A3\Soft_F_Exp\UGV_01\Data\UGV_01_int_olive_CO.paa","\A3\Data_F_Exp\Vehicles\Turret_olive_CO.paa"};
-    crew = QCLASS2(SIDE,Base);
-    typicalCargo[] = {};
 };
-//as of rn, drones are kinda broken, gotta figure that out lol
