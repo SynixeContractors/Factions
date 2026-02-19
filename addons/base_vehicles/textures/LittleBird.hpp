e5a7230ae6bd95c33ddc4c669543a30c0b823189
Patryk Kaniewski <p7tryk@gmail.com>
2026-02-19 15:38:10 +0100

    add melb

---
+++ addons/base_vehicles/textures/LittleBird.hpp
@@ -0,0 +1,12 @@
+class TextureSources {
+    class Black {
+        displayName = "Black";
+        textures[] = {"\x\tacs\addons\variants_melb\data\melb_black_ext_co.paa"};
+        factions[] = {};
+    };
+    class Theseus {
+        displayName = "Black";
+        textures[] = {"\x\tacs\addons\variants_melb\data\melb_theseus_black_ext_co.paa"};
+        factions[] = {};
+    };
+};
