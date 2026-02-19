166da84a705c460c1d677754bf002824392279b6
Patryk Kaniewski <p7tryk@gmail.com>
2026-02-19 15:44:26 +0100

    melb: camo

--- addons/base_vehicles/textures/LittleBird.hpp
+++ addons/base_vehicles/textures/LittleBird.hpp
@@ -1,6 +1,7 @@
 class TextureSources {
-    class Black {
-        displayName = "Black";
+    class Black_US {
+        //it says US army but i think we were using it anyways
+        displayName = "Black (US)";
         textures[] = {"\x\tacs\addons\variants_melb\data\melb_black_ext_co.paa"};
         factions[] = {};
     };
