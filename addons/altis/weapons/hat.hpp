cb345fa428dd7152077fe88638da754721593926
Patryk Kaniewski <p7tryk@gmail.com>
2026-02-20 15:41:43 +0100

    aaf: fix hat

--- addons/altis/weapons/hat.hpp
+++ addons/altis/weapons/hat.hpp
@@ -1,6 +1,6 @@
 @Launchers({
     "weapons": {
-        "launch_O_Vorona_brown_F": {
+        "launch_O_Vorona_green_F": {
             "magazinesBackpack": {
                 "Vorona_HEAT": 1,
             },
