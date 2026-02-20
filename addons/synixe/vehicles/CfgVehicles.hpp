1bd243ee1eb963387cb697b9aec48928d7ed79d5
Patryk Kaniewski <p7tryk@gmail.com>
2026-02-20 15:27:04 +0100

    synixe: add temporary camo for melb

--- addons/synixe/vehicles/CfgVehicles.hpp
+++ addons/synixe/vehicles/CfgVehicles.hpp
@@ -2,6 +2,7 @@ class CfgVehicles {
     #include "air\md500e.hpp"
     #define TEMPLATE_FACTION QPCLASS(t4_blufor)
     #define TEMPLATE_SIDE 1
+    #define TEMPLATE_CAMO Black_US
     #include "../../../templates/helicopters/LittleBird.hpp"
     class PCLASS(Contractor);
     class CLASS(HeliPilot): PCLASS(Contractor) {};
