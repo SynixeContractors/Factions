fe11da75b5c173b5a3004638819f23ffee64857a
Patryk Kaniewski <p7tryk@gmail.com>
2026-03-17 09:10:16 +0100

    stub

---
+++ addons/covenant/weapons/762.hpp
@@ -0,0 +1,64 @@
+@Primary({
+    "weapons": {
+        "Aegis_arifle_AK103_F": {
+            "probability": 1,
+            "magazinesVest": {
+                "30Rnd_762x39_polymer_Plum_Mag_Green_F": 4,
+            },
+        },
+        "Aegis_arifle_AK103_plum_F": {
+            "probability": 1,
+            "magazinesVest": {
+                "30Rnd_762x39_polymer_Plum_Mag_Green_F": 4,
+            },
+        },
+        "arifle_AK12_F": {
+            "probability": 1,
+            "magazinesVest": {
+                "30Rnd_762x39_AK12_Mag_F": 4,
+            },
+        },
+        "synixe_armoury_arifle_AK15_sand": {
+            "probability": 1,
+            "magazinesVest": {
+                "30Rnd_762x39_AK12_Mag_F": 4,
+            },
+        },
+        "arifle_AK12U_F": {
+            "probability": 1,
+            "magazinesVest": {
+                "30Rnd_762x39_AK12_Mag_F": 4,
+            },
+        },
+        "synixe_armoury_arifle_AK15K_sand": {
+            "probability": 1,
+            "magazinesVest": {
+                "30Rnd_762x39_AK12_Mag_F": 4,
+            },
+        },
+        "arifle_AKM_F": {
+            "probability": 1,
+            "magazinesVest": {
+                "30Rnd_762x39_AK12_Mag_F": 4,
+            },
+        },
+        "arifle_AKM_FL_F": {
+            "probability": 1,
+            "magazinesVest": {
+                "30Rnd_762x39_AK12_Mag_F": 4,
+            },
+        },
+        "synixe_armoury_arifle_Vektor762": {
+            "probability": 1,
+            "magazinesVest": {
+                "30Rnd_762x39_AK12_Mag_F": 4,
+            },
+        },
+        "synixe_armoury_arifle_Vektor762_sand": {
+            "probability": 1,
+            "magazinesVest": {
+                "30Rnd_762x39_AK12_Mag_F": 4,
+            },
+        },
+    },
+});
