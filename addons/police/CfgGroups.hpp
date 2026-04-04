d1288397c3232a6d3f857a467941e1d710f7d8ed
Brett Mayson <brett@mayson.io>
2026-04-04 10:52:35 -0600

    police groups

---
+++ addons/police/CfgGroups.hpp
@@ -0,0 +1,115 @@
+class CfgGroups {
+    @ForSides(OPFOR,BLUFOR,INDEP,CIV)
+    class $GROUP {
+        class CLASS(SIDE) {
+            name = "Police";
+            @For(Identities)
+            #define SIDE DOUBLES($1,$UPPER)
+            class CLASS2(SIDE,Patrol) {
+                name = "Patrol ($2)";
+                class CLASS2(SIDE,Patrol_Beat) {
+                    name = "Beat";
+                    faction = QCLASS($LOWER);
+                    side = $SIDE;
+                    __COUNTER_RESET__
+                    GROUP_UNIT(CLASS2(SIDE,PolicemanPistol),0);
+                    GROUP_UNIT(CLASS2(SIDE,PolicemanPistol),0);
+                };
+                class CLASS2(SIDE,Patrol_Patrol) {
+                    name = "Patrol";
+                    faction = QCLASS($LOWER);
+                    side = $SIDE;
+                    __COUNTER_RESET__
+                    GROUP_UNIT(CLASS2(SIDE,Policeman),0);
+                    GROUP_UNIT(CLASS2(SIDE,Policeman),0);
+                };
+                class CLASS2(SIDE,Patrol_ResponseTeam) {
+                    name = "Response Team";
+                    faction = QCLASS($LOWER);
+                    side = $SIDE;
+                    __COUNTER_RESET__
+                    GROUP_UNIT(CLASS2(SIDE,Policeman),0);
+                    GROUP_UNIT(CLASS2(SIDE,PolicemanSMG),0);
+                    GROUP_UNIT(CLASS2(SIDE,PolicemanPistol),0);
+                };
+            };
+            class CLASS2(SIDE,SWAT) {
+                name = "SWAT ($2)";
+                class CLASS2(SIDE,SWAT_Patrol) {
+                    name = "Patrol";
+                    faction = QCLASS($LOWER);
+                    side = $SIDE;
+                    __COUNTER_RESET__
+                    GROUP_UNIT(CLASS2(SIDE,SWAT),0);
+                    GROUP_UNIT(CLASS2(SIDE,SWAT),0);
+                };
+                class CLASS2(SIDE,SWAT_SniperTeam) {
+                    name = "Sniper Team";
+                    faction = QCLASS($LOWER);
+                    side = $SIDE;
+                    __COUNTER_RESET__
+                    GROUP_UNIT(CLASS2(SIDE,SWATSniper),0);
+                    GROUP_UNIT(CLASS2(SIDE,SWATSniper),0);
+                };
+                class CLASS2(SIDE,SWAT_ResponseTeam) {
+                    name = "Response Team";
+                    faction = QCLASS($LOWER);
+                    side = $SIDE;
+                    __COUNTER_RESET__
+                    GROUP_UNIT(CLASS2(SIDE,SWAT),0);
+                    GROUP_UNIT(CLASS2(SIDE,SWATRifle),0);
+                    GROUP_UNIT(CLASS2(SIDE,SWATRifle),0);
+                    GROUP_UNIT(CLASS2(SIDE,SWATDemo),0);
+                };
+                class CLASS2(SIDE,SWAT_EscortTeam) {
+                    name = "Escort Team";
+                    faction = QCLASS($LOWER);
+                    side = $SIDE;
+                    __COUNTER_RESET__
+                    GROUP_UNIT(CLASS2(SIDE,SWATSMG),0);
+                    GROUP_UNIT(CLASS2(SIDE,SWATSMG),0);
+                };
+            };
+            class CLASS2(SIDE,Gendarmerie) {
+                name = "Gendarmerie ($2)";
+                class CLASS2(SIDE,Gendarmerie_Patrol) {
+                    name = "Patrol";
+                    faction = QCLASS($LOWER);
+                    side = $SIDE;
+                    __COUNTER_RESET__
+                    GROUP_UNIT(CLASS2(SIDE,Gendarme),0);
+                    GROUP_UNIT(CLASS2(SIDE,Gendarme),0);
+                };
+                class CLASS2(SIDE,Gendarmerie_SniperTeam) {
+                    name = "Sniper Team";
+                    faction = QCLASS($LOWER);
+                    side = $SIDE;
+                    __COUNTER_RESET__
+                    GROUP_UNIT(CLASS2(SIDE,GendarmeSniper),0);
+                    GROUP_UNIT(CLASS2(SIDE,GendarmeSniper),0);
+                };
+                class CLASS2(SIDE,Gendarmerie_ResponseTeam) {
+                    name = "Response Team";
+                    faction = QCLASS($LOWER);
+                    side = $SIDE;
+                    __COUNTER_RESET__
+                    GROUP_UNIT(CLASS2(SIDE,Gendarme),0);
+                    GROUP_UNIT(CLASS2(SIDE,GendarmeRifle),0);
+                    GROUP_UNIT(CLASS2(SIDE,GendarmeRifle),0);
+                    GROUP_UNIT(CLASS2(SIDE,GendarmeBreacher),0);
+                };
+                class CLASS2(SIDE,Gendarmerie_EscortTeam) {
+                    name = "Escort Team";
+                    faction = QCLASS($LOWER);
+                    side = $SIDE;
+                    __COUNTER_RESET__
+                    GROUP_UNIT(CLASS2(SIDE,GendarmeSMG),0);
+                    GROUP_UNIT(CLASS2(SIDE,GendarmeSMG),0);
+                };
+            };
+            #undef SIDE
+            @EndFor
+        };
+    };
+    @EndForSides
+};
