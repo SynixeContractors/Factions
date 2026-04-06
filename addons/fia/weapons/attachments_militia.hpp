aa6f9620b78868725cee9ca8f26736d2013cf453
Patryk Kaniewski <p7tryk@gmail.com>
2026-04-06 19:05:09 +0200

    fia militia weapons

---
+++ addons/fia/weapons/attachments_militia.hpp
@@ -0,0 +1,19 @@
+@Primary({
+    "pointers": {
+        "JCA_acc_flashlight_tactical_black": 5,
+        "": 2,
+    },
+    "optics": {
+        "": 15,
+        "optic_Holosight_blk_F": 1,
+        "JCA_optic_ACOG_black": 1,
+        "JCA_optic_AHO_black": 1,
+        "JCA_optic_ARO_black": 1,
+        "JCA_optic_ICO_black": 1,
+        "JCA_optic_IHO_black": 1,
+        "JCA_optic_IHO_black_magnifier": 1,
+        "optic_MRCO": 1,
+        "JCA_optic_MRCS_black": 1,
+        "JCA_optic_MROS_black": 1,
+    },
+});
