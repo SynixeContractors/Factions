f6099d1ff4e403e356c6b686e2fb3b2d45b7bcd7
Patryk Kaniewski <p7tryk@gmail.com>
2026-04-07 12:29:10 +0200

    ram and lut

--- addons/fia/army_combat/CfgVehicles_Cars.hpp
+++ addons/fia/army_combat/CfgVehicles_Cars.hpp
@@ -18,13 +18,89 @@ class CLASS2(SIDE,BTR_SPG): GCLASS(BTR_SPG) {
     crew = QCLASS2(SIDE,Rifleman);
     typicalCargo[] = {QCLASS2(SIDE,Rifleman)};
 };
+
+#define RAM_MILITARY_TEXTURES textureList[] = {\
+  "AAF",1,\
+}
+
+//military styled rams
 class CLASS2(SIDE,Ram_MMG): GCLASS(Ram_MMG) {
     SCOPE_DLC;
     faction = QPCLASS(SIDE_FACTION);
     side = SIDE_NUMBER;
     crew = QCLASS2(SIDE,Rifleman);
     typicalCargo[] = {QCLASS2(SIDE,Rifleman)};
-    textureList[] = {
-        "AAF",1,
-    };
+    RAM_MILITARY_TEXTURES;
 };
+class CLASS2(SIDE,Ram_RCWS): GCLASS(Ram_RCWS) {
+    SCOPE_DLC;
+    faction = QPCLASS(SIDE_FACTION);
+    side = SIDE_NUMBER;
+    crew = QCLASS2(SIDE,Rifleman);
+    typicalCargo[] = {QCLASS2(SIDE,Rifleman)};
+    RAM_MILITARY_TEXTURES;
+};
+class CLASS2(SIDE,Ram_M134): GCLASS(Ram_M134) {
+    SCOPE_DLC;
+    faction = QPCLASS(SIDE_FACTION);
+    side = SIDE_NUMBER;
+    crew = QCLASS2(SIDE,Rifleman);
+    typicalCargo[] = {QCLASS2(SIDE,Rifleman)};
+    RAM_MILITARY_TEXTURES;
+};
+class CLASS2(SIDE,Ram_Comms): GCLASS(Ram_Comms) {
+    SCOPE_DLC;
+    faction = QPCLASS(SIDE_FACTION);
+    side = SIDE_NUMBER;
+    crew = QCLASS2(SIDE,Rifleman);
+    typicalCargo[] = {QCLASS2(SIDE,Rifleman)};
+    RAM_MILITARY_TEXTURES;
+};
+class CLASS2(SIDE,Ram_MRL): GCLASS(Ram_MRL) {
+    SCOPE_DLC;
+    faction = QPCLASS(SIDE_FACTION);
+    side = SIDE_NUMBER;
+    crew = QCLASS2(SIDE,Rifleman);
+    typicalCargo[] = {QCLASS2(SIDE,Rifleman)};
+    RAM_MILITARY_TEXTURES;
+};
+class CLASS2(SIDE,Ram_AA): GCLASS(Ram_AA) {
+    SCOPE_DLC;
+    faction = QPCLASS(SIDE_FACTION);
+    side = SIDE_NUMBER;
+    crew = QCLASS2(SIDE,Rifleman);
+    typicalCargo[] = {QCLASS2(SIDE,Rifleman)};
+    RAM_MILITARY_TEXTURES;
+};
+class CLASS2(SIDE,Ram_AT): GCLASS(Ram_AT) {
+    SCOPE_DLC;
+    faction = QPCLASS(SIDE_FACTION);
+    side = SIDE_NUMBER;
+    crew = QCLASS2(SIDE,Rifleman);
+    typicalCargo[] = {QCLASS2(SIDE,Rifleman)};
+    RAM_MILITARY_TEXTURES;
+};
+
+#undef RAM_MILITARY_TEXTURES
+#define LUT_MILITARY_TEXTURES textureList[] = {\
+  "Indep",1,\
+}
+
+class CLASS2(SIDE,LUT_Ammo): GCLASS(LUT_Ammo) {
+    SCOPE_DLC;
+    faction = QPCLASS(SIDE_FACTION);
+    side = SIDE_NUMBER;
+    crew = QCLASS2(SIDE,Rifleman);
+    typicalCargo[] = {QCLASS2(SIDE,Rifleman)};
+    LUT_MILITARY_TEXTURES;
+};
+class CLASS2(SIDE,LUT_MRL): GCLASS(LUT_MRL) {
+    SCOPE_DLC;
+    faction = QPCLASS(SIDE_FACTION);
+    side = SIDE_NUMBER;
+    crew = QCLASS2(SIDE,Rifleman);
+    typicalCargo[] = {QCLASS2(SIDE,Rifleman)};
+    LUT_MILITARY_TEXTURES;
+};
+
+#undef LUT_MILITARY_TEXTURES
--- addons/fia/militia/CfgVehicles_Cars.hpp
+++ addons/fia/militia/CfgVehicles_Cars.hpp
@@ -57,5 +57,83 @@ class CLASS2(SIDE,Ram_Rocket): GCLASS(Ram_Rocket) {
     typicalCargo[] = {QCLASS2(SIDE,Rifleman)};
     RAM_CIVILIAN_TEXTURES;
 };
-
 #undef RAM_CIVILIAN_TEXTURES
+
+
+#define LUT_CIVILIAN_TEXTURES textureList[] = {\
+  "Indep",4,\
+  "Black",1,\
+  "Blue",1,\
+  "Brown",1,\
+  "Green",1,\
+  "Lime",1,\
+  "Orange",1,\
+  "Sand",1,\
+  "Yellow",1,\
+}
+
+class CLASS2(SIDE,LUT_Transport): GCLASS(LUT_Transport) {
+    SCOPE_DLC;
+    faction = QPCLASS(SIDE_FACTION);
+    side = SIDE_NUMBER;
+    crew = QCLASS2(SIDE,Rifleman);
+    typicalCargo[] = {QCLASS2(SIDE,Rifleman)};
+    LUT_CIVILIAN_TEXTURES;
+};
+class CLASS2(SIDE,LUT_TransportCovered): GCLASS(LUT_TransportCovered) {
+    SCOPE_DLC;
+    faction = QPCLASS(SIDE_FACTION);
+    side = SIDE_NUMBER;
+    crew = QCLASS2(SIDE,Rifleman);
+    typicalCargo[] = {QCLASS2(SIDE,Rifleman)};
+    LUT_CIVILIAN_TEXTURES;
+};
+class CLASS2(SIDE,LUT_Repair): GCLASS(LUT_Repair) {
+    SCOPE_DLC;
+    faction = QPCLASS(SIDE_FACTION);
+    side = SIDE_NUMBER;
+    crew = QCLASS2(SIDE,Rifleman);
+    typicalCargo[] = {QCLASS2(SIDE,Rifleman)};
+    LUT_CIVILIAN_TEXTURES;
+};
+class CLASS2(SIDE,LUT_Medical): GCLASS(LUT_Medical) {
+    SCOPE_DLC;
+    faction = QPCLASS(SIDE_FACTION);
+    side = SIDE_NUMBER;
+    crew = QCLASS2(SIDE,Rifleman);
+    typicalCargo[] = {QCLASS2(SIDE,Rifleman)};
+    LUT_CIVILIAN_TEXTURES;
+};
+class CLASS2(SIDE,LUT_Fuel): GCLASS(LUT_Fuel) {
+    SCOPE_DLC;
+    faction = QPCLASS(SIDE_FACTION);
+    side = SIDE_NUMBER;
+    crew = QCLASS2(SIDE,Rifleman);
+    typicalCargo[] = {QCLASS2(SIDE,Rifleman)};
+    LUT_CIVILIAN_TEXTURES;
+};
+class CLASS2(SIDE,LUT_Cargo): GCLASS(LUT_Cargo) {
+    SCOPE_DLC;
+    faction = QPCLASS(SIDE_FACTION);
+    side = SIDE_NUMBER;
+    crew = QCLASS2(SIDE,Rifleman);
+    typicalCargo[] = {QCLASS2(SIDE,Rifleman)};
+    LUT_CIVILIAN_TEXTURES;
+};
+class CLASS2(SIDE,LUT_Flatbed): GCLASS(LUT_Flatbed) {
+    SCOPE_DLC;
+    faction = QPCLASS(SIDE_FACTION);
+    side = SIDE_NUMBER;
+    crew = QCLASS2(SIDE,Rifleman);
+    typicalCargo[] = {QCLASS2(SIDE,Rifleman)};
+    LUT_CIVILIAN_TEXTURES;
+};
+class CLASS2(SIDE,LUT_AA): GCLASS(LUT_AA) {
+    SCOPE_DLC;
+    faction = QPCLASS(SIDE_FACTION);
+    side = SIDE_NUMBER;
+    crew = QCLASS2(SIDE,Rifleman);
+    typicalCargo[] = {QCLASS2(SIDE,Rifleman)};
+    LUT_CIVILIAN_TEXTURES;
+};
+#undef LUT_CIVILIAN_TEXTURES
