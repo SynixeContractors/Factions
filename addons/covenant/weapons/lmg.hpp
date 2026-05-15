8a472edb06dae40740fbc057ad85f512f2386f18
Patryk Kaniewski <p7tryk@gmail.com>
2026-05-15 23:18:09 +0200

    headwear and backpacks

--- addons/covenant/weapons/lmg.hpp
+++ addons/covenant/weapons/lmg.hpp
@@ -3,157 +3,151 @@
         "Aegis_arifle_M4A1_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "Aegis_arifle_M4A1_short_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "Aegis_arifle_M4A1_grip_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "arifle_Mk20_black_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "arifle_Mk20_plain_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_Sand_green": 7,
-            },
-        },
-        "atlas_arifle_famasF1_F": {
-            "probability": 1,
-            "magazinesVest": {
-                "Atlas_25Rnd_556x45_Famas": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "atlas_arifle_famasF1_Grip_F": {
             "probability": 1,
             "magazinesVest": {
-                "Atlas_25Rnd_556x45_Famas": 7,
+                "Atlas_25Rnd_556x45_Famas": 5,
             },
         },
         "Atlas_Arifle_famasG2_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "Atlas_Arifle_famasG2_Grip_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "arifle_SPAR_02_blk_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "arifle_SPAR_02_snd_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_Sand_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "arifle_SPAR_01_blk_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "arifle_SPAR_01_snd_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_Sand_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "Aegis_arifle_M16A4_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "Aegis_arifle_M16A4_FG_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "arifle_SCAR_L_black_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "arifle_SCAR_L_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_Sand_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "arifle_SCAR_L_grip_black_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "arifle_SCAR_L_grip_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_Sand_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "arifle_SCAR_L_short_black_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "arifle_SCAR_L_short_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "arifle_TRG21_black_F": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "arifle_XMS_lxWS": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
         "arifle_XMS_Gray_lxWS": {
             "probability": 1,
             "magazinesVest": {
-                "30Rnd_556x45_Stanag_green": 7,
+                "75Rnd_556x45_Stanag_green_lxWS": 5,
             },
         },
-        "arifle_VelkoR5_lxWS": {
+        "arifle_Velko_lxWS": {
             "probability": 1,
             "magazinesVest": {
-                "35Rnd_556x45_Velko_reload_tracer_red_lxWS": 7,
+                "50Rnd_556x45_Velko_tracer_green_lxWS": 5,
             },
         },
-        "Aegis_arifle_VelkoR5_sand": {
+        "Aegis_arifle_Velko_sand": {
             "probability": 1,
             "magazinesVest": {
-                "35Rnd_556x45_Velko_reload_tracer_red_lxWS": 7,
+                "50Rnd_556x45_Velko_tracer_green_lxWS": 5,
             },
         },
     },
