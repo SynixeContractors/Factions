080d2b0474349ffdb6fec185fce0bdacaa93cc46
Patryk Kaniewski <p7tryk@gmail.com>
2026-05-15 22:20:05 +0200

    winter civilians

---
+++ addons/covenant/wear/armor.hpp
@@ -0,0 +1,13 @@
+@Vests({
+    "variants": {
+        "synixe_mgp_vest_battle_belt_assaulter_blk": 1,
+        "synixe_mgp_vest_battle_belt_assaulter_cb": 1,
+        "synixe_mgp_vest_battle_belt_assaulter_khk": 1,
+        "synixe_mgp_vest_battle_belt_hgunner_blk": 1,
+        "synixe_mgp_vest_battle_belt_hgunner_cb": 1,
+        "synixe_mgp_vest_battle_belt_hgunner_khk": 1,
+    },
+    "packs": [
+        "t4_standard",
+    ],
+});
