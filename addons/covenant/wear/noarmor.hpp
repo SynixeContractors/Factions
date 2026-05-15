28d515cbc22cda6b6136c10083b237056507a08e
Patryk Kaniewski <p7tryk@gmail.com>
2026-05-15 22:20:05 +0200

    winter civilians

---
+++ addons/covenant/wear/noarmor.hpp
@@ -0,0 +1,21 @@
+@Vests({
+    "variants": {
+        "V_Rangemaster_belt_blk": 1,
+        "V_Rangemaster_belt_cbr": 1,
+        "V_Rangemaster_belt_oli": 1,
+        "V_Rangemaster_belt_khk": 1,
+        "Aegis_V_CarrierRigKBT_01_holster_black_F": 1,
+        "Aegis_V_CarrierRigKBT_01_holster_cbr_F": 1,
+        "Aegis_V_CarrierRigKBT_01_holster_khk_F": 1,
+        "Aegis_V_CarrierRigKBT_01_holster_olive_F": 1,
+        "V_LegStrapBag_black_F": 1,
+        "V_LegStrapBag_coyote_F": 1,
+        "V_LegStrapBag_olive_F": 1,
+        "V_BandollierB_blk": 1,
+        "V_BandollierB_rgr": 1,
+        "V_BandollierB_oli": 1,
+    },
+    "packs": [
+        "t4_standard",
+    ],
+});
