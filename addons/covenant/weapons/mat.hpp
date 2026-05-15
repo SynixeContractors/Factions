aeb609886fd790123bc7eb409c2ec1b48fd67558
Patryk Kaniewski <p7tryk@gmail.com>
2026-05-15 23:31:20 +0200

    heavy weapons

---
+++ addons/covenant/weapons/mat.hpp
@@ -0,0 +1,20 @@
+@Launchers({
+    "weapons": {
+        "Aegis_launch_RPG7M_F": {
+            "probability": 0.8,
+            "magazinesBackpack": {
+                "RPG7_F": 2,
+                "PSRL1_HE_RF": 1,
+            },
+            "loadedPrimary": "RPG7_F",
+        },
+        "launch_PSRL1_black_RF": {
+            "probability": 0.2,
+            "magazinesBackpack": {
+                "PSRL1_AT_RF": 2,
+                "PSRL1_HE_RF": 1,
+            },
+            "loadedPrimary": "PSRL1_AT_RF",
+        },
+    },
+});
