4d2812acf08f946fd802609370aeea3bc4a8a25e
TWatson1162 <mildlyperturbedgamer25@gmail.com>
2026-05-18 17:32:00 +0900

    Facewear fix
    removes mgp facewear, replaced with balaclava w/ no glasses cause arma faces are puffy

--- addons/yongshi/headgear/Full.hpp
+++ addons/yongshi/headgear/Full.hpp
@@ -13,6 +13,8 @@
     "JCA_G_balaclava_01_glasses_black_F": 1,
     "JCA_G_balaclava_01_glasses_sand_F": 1,
     "JCA_G_balaclava_01_glasses_olive_F": 1,
-    "synixe_mgp_f_face_shield_blk_tactical": 1,
-    "synixe_mgp_f_face_shield_rgr_tactical": 1,
+    "JCA_G_balaclava_01_black_F": 1,
+    "JCA_G_balaclava_01_sand_F": 1,
+    "JCA_G_balaclava_01_olive_F": 1,
+
 });
--- addons/yongshi/headgear/Light.hpp
+++ addons/yongshi/headgear/Light.hpp
@@ -9,4 +9,7 @@
     "JCA_G_balaclava_01_glasses_black_F": 1,
     "JCA_G_balaclava_01_glasses_sand_F": 1,
     "JCA_G_balaclava_01_glasses_olive_F": 1,
+    "JCA_G_balaclava_01_black_F": 1,
+    "JCA_G_balaclava_01_sand_F": 1,
+    "JCA_G_balaclava_01_olive_F": 1,
 });
