6a2cf854a52d97aff8631f456bdabdc18eb9c5fb
TWatson1162 <mildlyperturbedgamer25@gmail.com>
2026-05-21 13:46:06 +0900

    Minor config fix

--- addons/tiyen/CfgGroups.hpp
+++ addons/tiyen/CfgGroups.hpp
@@ -1,7 +1,7 @@
 class CfgGroups {
     class EAST {
         class CLASS(t3_opfor) {
-            name = "[T3] Tiyen";
+            name = "{CSP} [T3] Tiyen";
             @Groups(T3,OPFOR,Army_Combat);
             @Groups(T3,OPFOR,Army_Garrison);
             @Groups(T3,OPFOR,Reserves);
