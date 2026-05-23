7ad5c2221629ee0894186efb0f83ff623417b152
TWatson1162 <mildlyperturbedgamer25@gmail.com>
2026-05-23 12:33:21 +0900

    Removed OPFOR from CfgGroups

--- addons/yongshi/CfgGroups.hpp
+++ addons/yongshi/CfgGroups.hpp
@@ -2,7 +2,7 @@ class CfgGroups {
     class EAST {
         class CLASS(t3_opfor) {
             name = "[T3] Yong Shì Mercenaries";
-            @Groups(T3,OPFOR,Army_Combat);
+            @Groups(T3,Army_Combat);
         };
     };
 };
