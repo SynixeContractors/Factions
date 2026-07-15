f24da6b258252e4b5f5ddc850a7eb9dfb262c885
HarmfulBug <74442597+TWatson1162@users.noreply.github.com>
2026-07-15 19:57:38 +0900

    Yong Shi Mercs (#206)
    * Yongshi Mercs
    
    Pretty much just copied over one of the old factions (which I originally was inspired to make from Black Powder Red Earth -- Honbin Mercs) that I made in the synixe units mod before making this faction mod, only real change is the camo  (AOR1 to splotch or whatever tf its called) -- uses Tiyen identity which I readded since it's not on main by the time this is added, but it is not a CSP faction.
    
    Put simply, this faction is made of veterans of Tiyen and other "Asian" (whatever that means in this universe) countries. Their main goal is to work with insurgencies of resource rich countries as advisors/enablers for their employeers. I only really put them as Army Combat for the machinegunner role and nothing else cause I was too lazy to make an "Advisors" subcategory.
    
    * Facewear fix
    
    removes mgp facewear, replaced with balaclava w/ no glasses cause arma faces are puffy
    
    * fix arco and add loadedPrimary to MAT
    
    * Added boxes to Assistant MG Backpack
    
    * Removed OPFOR from CfgGroups
    
    * Getting rid of spaces
    
    * space fix
    
    ---------
    
    Co-authored-by: BrettMayson <brett@mayson.io>

---
+++ addons/yongshi/headgear/Helmet.hpp
@@ -0,0 +1,16 @@
+@Headgear({
+    "synixe_mgp_helmet_airframe_01_khk": 1,
+    "synixe_mgp_helmet_airframe_01_rgr_hexagon": 1,
+    "synixe_mgp_helmet_airframe_01_rgr": 1,
+    "synixe_mgp_helmet_airframe_01_cb_hexagon": 1,
+    "synixe_mgp_helmet_airframe_01_cb": 1,
+});
+
+@Facewear({
+    "JCA_G_balaclava_01_glasses_black_F": 1,
+    "JCA_G_balaclava_01_glasses_sand_F": 1,
+    "JCA_G_balaclava_01_glasses_olive_F": 1,
+    "JCA_G_balaclava_01_black_F": 1,
+    "JCA_G_balaclava_01_sand_F": 1,
+    "JCA_G_balaclava_01_olive_F": 1,
+});
