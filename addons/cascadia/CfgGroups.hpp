f15e8c0b68658805420aa038f5517cf4aa246e46
TWatson1162 <mildlyperturbedgamer25@gmail.com>
2026-08-11 13:36:21 +0900

    T2 to T3 on GfgGroups.hpp

--- addons/cascadia/CfgGroups.hpp
+++ addons/cascadia/CfgGroups.hpp
@@ -2,10 +2,10 @@ class CfgGroups {
     class EAST {
         class CLASS(t3_opfor) {
             name = "{CSP} [T3] Cascadia";
-            @Groups(T2,OPFOR,Army_Combat);
-            @Groups(T2,OPFOR,Army_Recon);
-            @Groups(T2,OPFOR,Army_Garrison);
-            @Groups(T2,OPFOR,Reserves);
+            @Groups(T3,OPFOR,Army_Combat);
+            @Groups(T3,OPFOR,Army_Recon);
+            @Groups(T3,OPFOR,Army_Garrison);
+            @Groups(T3,OPFOR,Reserves);
         };
     };
 };
