d8510db96113a5d4eb7e79b66d3d56c45dabe0e5
Patryk Kaniewski <p7tryk@gmail.com>
2026-08-11 18:26:06 +0200

    add ghosthawk DAP and assault from 2.22 (#220)

--- templates/helicopters/Ghosthawk_Armed.hpp
+++ templates/helicopters/Ghosthawk_Armed.hpp
@@ -24,3 +24,29 @@ class CLASS(Ghosthawk_Pylons): GCLASS(Ghosthawk_Pylons) {
     textureList[] = {QUOTE(TEMPLATE_CAMO),1};
     #endif
 };
+class GCLASS(Ghosthawk_Assault);
+class CLASS(Ghosthawk_Assault): GCLASS(Ghosthawk_Assault) {
+    SCOPE_DLC;
+    faction = TEMPLATE_FACTION;
+    side = TEMPLATE_SIDE;
+    crew = QCLASS(HeliPilot);
+    typicalCargo[] = {QCLASS(HeliPilot)};
+    #ifdef TEMPLATE_CAMO_LIST
+    textureList[] = TEMPLATE_CAMO_LIST;
+    #else
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+    #endif
+};
+class GCLASS(Ghosthawk_DAP);
+class CLASS(Ghosthawk_DAP): GCLASS(Ghosthawk_DAP) {
+    SCOPE_DLC;
+    faction = TEMPLATE_FACTION;
+    side = TEMPLATE_SIDE;
+    crew = QCLASS(HeliPilot);
+    typicalCargo[] = {QCLASS(HeliPilot)};
+    #ifdef TEMPLATE_CAMO_LIST
+    textureList[] = TEMPLATE_CAMO_LIST;
+    #else
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+    #endif
+};
