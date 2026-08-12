d563fc3d6c2717cbc3bbf3abfeb9ab104a76d6f6
Patryk Kaniewski <p7tryk@gmail.com>
2026-08-12 15:48:51 +0200

    armor no armor covenant

--- addons/covenant/wear/armor.hpp
+++ addons/covenant/wear/armor.hpp
@@ -1,5 +1,11 @@
 @Vests({
     "variants": {
+        "V_TacVest_grn": 5,
+        "V_TacVest_oli": 5,
+        "V_lxWS_TacVestIR_oli": 2,
+        "V_lxWS_TacVestIR_oli": 2,
+        "V_PlateCarrierIA1_grn": 1,
+        "V_PlateCarrierIA1_grn": 1,
         "synixe_mgp_vest_battle_belt_assaulter_blk": 1,
         "synixe_mgp_vest_battle_belt_assaulter_cb": 1,
         "synixe_mgp_vest_battle_belt_assaulter_khk": 1,
--- addons/covenant/wear/noarmor.hpp
+++ addons/covenant/wear/noarmor.hpp
@@ -14,6 +14,12 @@
         "V_BandollierB_blk": 1,
         "V_BandollierB_rgr": 1,
         "V_BandollierB_oli": 1,
+        "synixe_mgp_vest_battle_belt_assaulter_blk": 1,
+        "synixe_mgp_vest_battle_belt_assaulter_cb": 1,
+        "synixe_mgp_vest_battle_belt_assaulter_khk": 1,
+        "synixe_mgp_vest_battle_belt_hgunner_blk": 1,
+        "synixe_mgp_vest_battle_belt_hgunner_cb": 1,
+        "synixe_mgp_vest_battle_belt_hgunner_khk": 1,
     },
     "packs": [
         "t4_standard",
