d51546458ce3b722a8458e16ca8725e9e60ec19a
LtHave <141883390+LtHave@users.noreply.github.com>
2026-08-22 16:59:05 +0200

    Adds vehicles to Breznia
    - Adds Pandur
    - Adds BMP variants
    - Adds BTR variants
    - Adds Badger variants
    - Adds Warrior
    - Adds Ram variants
    - Adds M-ATV variants
    - Adds Polaris variants
    - Adds Fennek (unarmed)
    - Adds LUT variants
    - Adds Quadbike
    - Adds Leopard 2
    - Adds T-100
    - Adds SU-75 variants
    - Adds YAK-130
    - Adds C-130
    - Adds Mi-35
    - Adds Ka-54
    - Adds Orca variants

---
+++ addons/breznia/army_combat/CfgVehicles_Planes.hpp
@@ -0,0 +1,50 @@
+class GCLASS(SU75);
+class CLASS(SU75): GCLASS(SU75) {
+    SCOPE_DLC;
+    faction = QPCLASS(t3_indep);
+    side = 2;
+    textureList[] = {"RUBlue",1};
+    editorPreview = "\A3\EditorPreviews_F_Jets\Data\Cfgvehicles\O_Plane_Fighter_02_F.jpg";
+    crew = QCLASS(FighterPilot);
+};
+
+class GCLASS(SU75_Stealth);
+class CLASS(SU75_Stealth): GCLASS(SU75_Stealth) {
+    SCOPE_DLC;
+    faction = QPCLASS(t3_indep);
+    side = 2;
+    textureList[] = {"RUBlue",1};
+    editorPreview = "\A3\EditorPreviews_F_Jets\Data\Cfgvehicles\O_Plane_Fighter_02_F.jpg";
+    crew = QCLASS(FighterPilot);
+};
+
+class GCLASS(Yak130);
+class CLASS(Yak130): GCLASS(Yak130) {
+    SCOPE_DLC;
+    faction = QPCLASS(t3_indep);
+    side = 2;
+    textureList[] = {"RUBlue",1};
+    editorPreview = "\A3\EditorPreviews_F\Data\CfgVehicles\O_Plane_CAS_02_dynamicLoadout_F.jpg";
+    crew = QCLASS(FighterPilot);
+};
+
+class GCLASS(Shahpar);
+class CLASS(Shahpar): GCLASS(Shahpar) {
+    SCOPE_DLC;
+    faction = QPCLASS(t3_indep);
+    side = 2;
+    textureList[] = {"Rus",1};
+    editorPreview = "\A3\EditorPreviews_F\Data\CfgVehicles\O_UAV_02_dynamicLoadout_F.jpg";
+    crew = QCLASS(FighterPilot);
+};
+
+class GCLASS(C130);
+class CLASS(C130): GCLASS(C130) {
+    SCOPE_DLC;
+    faction = QPCLASS(t3_indep);
+    side = 2;
+    textureList[] = {"GenBlue",1};
+    editorPreview = "\A3\EditorPreviews_F\Data\CfgVehicles\I_MBT_03_cannon_F.jpg";
+    crew = QCLASS(HeliPilot);
+    typicalCargo[] = {QCLASS(HeliPilot), QCLASS(HeliPilot), QCLASS(HeliPilot)};
+};
