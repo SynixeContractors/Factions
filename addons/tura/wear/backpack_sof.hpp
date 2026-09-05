73bd558727d49104efb0411dfa14418f7331e785
Patryk Kaniewski <p7tryk@gmail.com>
2026-09-05 14:33:27 +0200

    Tura Insurgency (#200)
    * tura stub
    
    * add international volunteers
    
    * tura marksman and mat fixes
    
    * flesh out SF
    
    * fix riflemancarabine
    
    * fix sf groups
    
    * merge
    
    * fix btr camo
    
    ---------
    
    Co-authored-by: Brett Mayson <brett@mayson.io>

---
+++ addons/tura/wear/backpack_sof.hpp
@@ -0,0 +1,9 @@
+@Backpacks({
+    "variants": {
+        "B_AssaultPack_cbr": 1,
+        "B_AssaultPackSpec_cbr": 1,
+        "B_AssaultPack_mcamo": 1,
+        "B_MU_AssaultPack_US_mcamo": 1,
+        "B_LegStrapBag_coyote_F": 1,
+    },
+});
