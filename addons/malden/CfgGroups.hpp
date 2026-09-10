8cfa943d8c0743a2c7cf2121032c4a65294f367c
Patryk Kaniewski <p7tryk@gmail.com>
2026-09-11 00:09:41 +0200

    fix groups

--- addons/malden/CfgGroups.hpp
+++ addons/malden/CfgGroups.hpp
@@ -2,8 +2,8 @@ class CfgGroups {
     class INDEP {
         class CLASS(t3_indep) {
             name = FACTION_NAME;
-            @Groups(T3,INDEP,Army_Combat);
-            @Groups(T3,INDEP,Army_Garrison);
+            @Groups(T3,Army_Combat);
+            @Groups(T3,Army_Garrison);
         };
     };
 };
