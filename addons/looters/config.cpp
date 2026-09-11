8e2d4f53b45c9fb2c946f3131e083f5dbf671483
Brett Mayson <brett@mayson.io>
2026-09-11 13:31:00 -0600

    fix looter addon

--- addons/looters/config.cpp
+++ addons/looters/config.cpp
@@ -1,8 +1,8 @@
 #include "script_component.hpp"
 
 class CfgPatches {
-    class DOUBLES(ADDON,SUBCOMPONENT) {
-        name = QUOTE(DOUBLES(COMPONENT,SUBCOMPONENT));
+    class ADDON {
+        name = QUOTE(COMPONENT);
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
