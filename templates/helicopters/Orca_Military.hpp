fd24ea4a4eef8319b5c91dc33078a54e2b85b278
Patryk Kaniewski <p7tryk@gmail.com>
2026-09-20 00:19:45 +0200

    add missing non armored orca attack to template (#238)

--- templates/helicopters/Orca_Military.hpp
+++ templates/helicopters/Orca_Military.hpp
@@ -24,3 +24,16 @@ class CLASS(OrcaR): GCLASS(OrcaR) {
     textureList[] = {QUOTE(TEMPLATE_CAMO),1};
     #endif
 };
+class GCLASS(OrcaArmed);
+class CLASS(OrcaArmed): GCLASS(OrcaArmed) {
+    SCOPE_DLC;
+    faction = TEMPLATE_FACTION;
+    side = TEMPLATE_SIDE;
+    crew = QCLASS(HeliPilot);
+    typicalCargo[] = {QCLASS(HeliPilot)};
+    #ifdef TEMPLATE_CAMO_LIST
+    textureList[] = TEMPLATE_CAMO_LIST;
+    #else
+    textureList[] = {QUOTE(TEMPLATE_CAMO),1};
+    #endif
+};
