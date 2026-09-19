9e737671bab7742aef4e8ee08228e23860003990
Patryk Kaniewski <p7tryk@gmail.com>
2026-09-19 11:28:51 +0200

    fix groups for new format

--- addons/cascadia/CfgGroups.hpp
+++ addons/cascadia/CfgGroups.hpp
@@ -2,10 +2,10 @@ class CfgGroups {
     class EAST {
         class CLASS(t3_opfor) {
             name = "{CSP} [T3] Cascadia";
-            @Groups(T3,OPFOR,Army_Combat);
-            @Groups(T3,OPFOR,Army_Recon);
-            @Groups(T3,OPFOR,Army_Garrison);
-            @Groups(T3,OPFOR,Reserves);
+            @Groups(T3,Army_Combat);
+            @Groups(T3,Army_Recon);
+            @Groups(T3,Army_Garrison);
+            @Groups(T3,Reserves);
         };
     };
 };
